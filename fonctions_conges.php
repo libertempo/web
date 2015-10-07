bda8e4b0a23d812d1a8a4d64c9b39d9b9495437a
wouldsmina <wouldsmina@gmail.com>
2015-10-07 21:20:44 +0200

    include_once pour fonction calcul
    Même si je doute que cela soit nécessaire, je le met au cas ou...

--- fonctions_conges.php
+++ fonctions_conges.php
@@ -2612,6 +2612,7 @@ $user_nb_jours_pris = number_format($user_nb_jours_pris, 1, '.', '');
 			//d'apres la data limite et ne pas le décompter des reliquats !!!
 			else
 			{
+				include_once 'fonctions_calcul.php' ;
 				$comment="calcul reliquat -> date limite" ;
 				$nb_reliquats_a_deduire = compter($user_login, $num_current_periode, $date_deb, $_SESSION['config']['date_limite_reliquats'], $demi_jour_deb, "pm", $comment ,  $DEBUG);
 
