2e567faa345d98eed2467b9bfcebf4c4131fdfcc
php-conges <wouldsmina@gmail.com>
2016-02-15 22:26:24 +0100

    Bienvenue à Quinnis

--- template/reboot/template_define.php
+++ template/reboot/template_define.php
@@ -1,6 +1,5 @@
 <?php
 
-define('LOGO','img/logo_um2_v.gif');
-define('BOTTOM_TEXT','Libertempo');
-// define('IMG_INDEX','img/vacances-mer.jpg');
-// define('HEADER_LOGIN','img/logo_um2_v.gif');
+define('BOTTOM_TEXT','Libertempo 1.8 "Quinnis". <a href="https://github.com/wouldsmina/Libertempo/issues">Un bug, une remarque <i class="fa fa-github fa-2x"></a></i>');
+
+
