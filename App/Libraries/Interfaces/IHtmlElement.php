a19d93d7521b006c1c9548c369b6bd352a6acb89
Prytoegrian <prytoegrian@protonmail.com>
2016-02-20 16:15:28 +0100

    Création des contrats

---
+++ App/Libraries/Interfaces/IHtmlElement.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Décrit le contrat d'un élément Html
+ *
+ * @since  1.9
+ * @author Prytoegrian <prytoegrian@protonmail.com>
+ */
+interface IHtmlElement
+{
+    /**
+     * Ajoute une classe à l'élément
+     *
+     * @param string $class
+     *
+     * @return void
+     * @since 1.9
+     */
+    public function addClass($class);
+
+    /**
+     * Ajoute un tableau de classes à l'élément
+     *
+     * @param array $classes
+     *
+     * @return void
+     * @since 1.9
+     */
+    public function addClasses(array $classes);
+
+    /**
+     * Ajoute un attribut quelconque à l'élément
+     *
+     *
+     * @param string $name
+     * @param string $value
+     *
+     * @return void
+     * @since 1.9
+     */
+    public function addAttribute($name, $value);
+
+    /**
+     * Ajoute une liste d'attributs quelconques à l'élément
+     * @example ['nomAttr1' => 'val1', ...]
+     *
+     * @param array $list
+     *
+     * @return void
+     * @since 1.9
+     */
+    public function addAttributes(array $list);
+
+    /**
+     * Renvoie l'id unique de l'élément
+     *
+     * @return void
+     * @since 1.9
+     */
+    public function getId();
+}
---
+++ App/Libraries/Interfaces/IRenderable.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Décrit le contrat d'un élément renderable
+ *
+ * @since  1.9
+ * @author Prytoegrian <prytoegrian@protonmail.com>
+ */
+interface IRenderable
+{
+    /**
+     * Render l'élément
+     *
+     * @return void
+     * @since 1.9
+     */
+    public function render();
+}
