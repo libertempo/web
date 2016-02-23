52b278d0eb4cb4fd10dc1467a25f438a9569b655
wouldsmina <wouldsmina@gmail.com>
2016-02-23 21:00:22 +0100

    config_ldap.php inutile. Fix #115

--- admin/admin_db_sauve.php
+++ admin/admin_db_sauve.php
@@ -30,7 +30,6 @@ require_once ROOT_PATH . 'define.php';
 
 $session=(isset($_GET['session']) ? $_GET['session'] : ((isset($_POST['session'])) ? $_POST['session'] : session_id()) ) ;
 
-include_once CONFIG_PATH .'config_ldap.php';
 include_once ROOT_PATH .'fonctions_conges.php' ;
 include_once INCLUDE_PATH .'fonction.php';
 include_once INCLUDE_PATH .'session.php';
