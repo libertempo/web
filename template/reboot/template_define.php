8fa214481db4b75496b66be8aa063d83671c1cac
php-conges <wouldsmina@gmail.com>
2016-04-01 21:40:49 +0200

    Libertempo 1.8 Quinnis stable

--- template/reboot/template_define.php
+++ template/reboot/template_define.php
@@ -1,5 +1,5 @@
 <?php
 
-define('BOTTOM_TEXT','Libertempo 1.8 "Quinnis". <a href="https://github.com/wouldsmina/Libertempo/issues">Un bug, une remarque <i class="fa fa-github fa-2x"></i></a>');
+define('BOTTOM_TEXT','Libertempo 1.8 Quinnis');
 
 
