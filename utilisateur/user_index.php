9bb7f5598d84132566694f5991c39146deaf7f3f
Prytoegrian <prytoegrian@protonmail.com>
2016-06-05 12:09:07 +0200

    Ordre onglets

--- utilisateur/user_index.php
+++ utilisateur/user_index.php
@@ -53,8 +53,6 @@ $onglets = array();
 
 if( $_SESSION['config']['user_saisie_demande'] || $_SESSION['config']['user_saisie_mission'] ) {
     $onglets['nouvelle_absence'] = _('divers_nouvelle_absence');
-    $onglets['ajout_heure_repos'] = _('divers_ajout_heure_repos');
-    $onglets['ajout_heure_additionnelle'] = _('divers_ajout_heure_additionnelle');
 }
 
 if( $_SESSION['config']['user_echange_rtt'] ) {
@@ -67,7 +65,13 @@ if( $_SESSION['config']['user_saisie_demande'] ) {
 
 $onglets['historique_conges'] = _('user_onglet_historique_conges');
 $onglets['historique_autres_absences'] = _('user_onglet_historique_abs');
+if( $_SESSION['config']['user_saisie_demande'] || $_SESSION['config']['user_saisie_mission'] ) {
+    $onglets['ajout_heure_repos'] = _('divers_ajout_heure_repos');
+}
 $onglets['liste_heure_repos'] = _('user_liste_heure_repos');
+if( $_SESSION['config']['user_saisie_demande'] || $_SESSION['config']['user_saisie_mission'] ) {
+    $onglets['ajout_heure_additionnelle'] = _('divers_ajout_heure_additionnelle');
+}
 $onglets['liste_heure_additionnelle'] = _('user_liste_heure_additionnelle');
 
 if( $_SESSION['config']['auth'] && $_SESSION['config']['user_ch_passwd'] ) {
