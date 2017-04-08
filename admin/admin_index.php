c25b688ad60c37a58e2ea45ea5af7f1105903f32
wouldsmina <wouldsmina@tuxfamily.org>
2017-04-09 00:47:26 +0200

    suppression des anciens onglets

--- admin/admin_index.php
+++ admin/admin_index.php
@@ -36,9 +36,6 @@ $onglets['ajout-user']    = _('admin_onglet_add_user');
 if( $_SESSION['config']['gestion_groupes'] ) {
     if( $_SESSION['config']['admin_see_all'] || $_SESSION['userlogin']=="admin" || is_hr($_SESSION['userlogin']) )
         $onglets['admin-group'] = _('admin_onglet_gestion_groupe');
-    $onglets['admin-group-users'] = _('admin_onglet_groupe_user');
-    if( $_SESSION['config']['admin_see_all'] || $_SESSION['userlogin']=="admin" || is_hr($_SESSION['userlogin']) )
-        $onglets['admin-group-responsables'] = _('admin_onglet_groupe_resp');
 }
 
 if ( !isset($onglets[ $onglet ]) && !in_array($onglet, array('chg_pwd_user', 'ajout_group', 'modif_group', 'modif_user', 'suppr_group','suppr_user')))
