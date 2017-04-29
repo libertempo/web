2660b124f5d50609367d997b5d0ffa783dad8791
Prytoegrian <Prytoegrian@users.noreply.github.com>
2017-04-30 00:05:18 +0200

    Sécurisation de l'API - Partie 1 (#370)
    * Màj / install vendor
    
    * LT API version fixe
    
    * Script duplication et accession à l'API vendor depuis le front
    
    * Oups config
    
    * Update composer
    
    * API en vendor (enfin)
    
    * Ajout des champs de conges_users pour la sécurité
    
    * Pryto, le roi des idiots (c'est comme les Pépito mais sans chapeau)
    
    * bépo, tu fais chier
    
    * Patch encore (la x° fois c'est la bonne)
    
    * SQL
    
    * Test
    
    * Ajout du token d'instance
    
    * Génération du token d'instance via le processus d'install / maj
    
    * Reset à chaque version
    
    * Json bien formé
    
    * Bon paths et compat windows
    
    * Mise à jour lib sql 1.10
    
    * pareil pour la maj
    
    * Suppression code déjà migré
    
    * Betîse ancienne lib sql
    
    * ...

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ include_once INCLUDE_PATH .'fonction.php';
 include_once ROOT_PATH .'fonctions_conges.php'; // for init_config_tab()
 $_SESSION['config']=init_config_tab();      // on initialise le tableau des variables de config
 
-
 /***** DEBUT DU PROG *****/
 
 /*** initialisation des variables ***/
