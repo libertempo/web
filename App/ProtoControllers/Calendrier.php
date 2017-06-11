8de66ad339a5b9c2f35cf02a9d04ddae4d7639e7
Prytoegrian <prytoegrian@protonmail.com>
2017-06-11 15:14:14 +0200

    Mise en place d'une vue mois

--- App/ProtoControllers/Calendrier.php
+++ App/ProtoControllers/Calendrier.php
@@ -6,294 +6,67 @@ namespace App\ProtoControllers;
  *
  * @since  1.9
  * @author Prytoegrian <prytoegrian@protonmail.com>
+ * @author Wouldsmina
  *
- * Ne doit contacter que \App\Libraries\Calendrier\BusinessCollection, \App\Libraries\Calendrier\Fournisseur
+ * Ne doit contacter que \App\Libraries\Calendrier\Facade
  * Ne doit être contacté que par la page en procédural
  */
 final class Calendrier
 {
     /**
-     * @var int
+    * @var \DateTimeInterface
      */
-    const VUE_MOIS = 1;
+    private $jourDemande;
 
     /**
-     * @var int
+    * @var \DateTimeInterface
      */
-    const VUE_SEMAINE = 2;
-
-    /**
-     * @var \DateTimeInterface Date de debut de la période courante
-     */
-    private $dateDebutMaintenant;
-
-    /**
-     * @var \DateTimeInterface Date de fin de la période courante
-     */
-    private $dateFinMaintenant;
-
-    /**
-     * @var \DateTimeInterface Date de début de récolte du calendrier
-     */
-    private $dateDebut;
-
-    /**
-     * @var \DateTimeInterface Date de fin de récolte du calendrier
-     */
-    private $dateFin;
-
-    /**
-     * @var \DateTimeInterface Date de début de période précédente
-     */
-    private $dateDebutPrecedente;
-
-    /**
-     * @var \DateTimeInterface Date de fin de période suivante
-     */
-    private $dateFinSuivante;
-
-    /**
-     * @var int Vue du calendrier à consulter
-     */
-    private $vue = self::VUE_MOIS;
-
-    /**
-     * @var int Id du groupe dont on veut voir les événements
-     */
-    private $idGroupe = NIL_INT;
-
-    public function __construct()
-    {
-        $this->session=(isset($_GET['session']) ? $_GET['session'] : ((isset($_POST['session'])) ? $_POST['session'] : session_id()));
-
-        if(substr($this->session, 0, 9)!="phpconges") {
-            session_start();
-            $_SESSION['config']=init_config_tab();
-            if(empty($_SESSION['userlogin'])) {
-                redirect( ROOT_PATH . 'index.php' );
-            }
-        } else {
-            include_once INCLUDE_PATH . 'session.php';
-        }
-    }
-
-    /*
-     * récupérer les evenements qui sont associés à l'utilisateur
-     * le pattern facade ferait bien le job, mais il faut éviter au maximum a création massive d'objet
-     *
-     * je ne sais pas quelle structure de données faire pour avoir de la rapidité sur l'exécution,
-     * de la souplesse sur l'utilisation et de la fiabilité
-     */
-
-    /**
-     * Retourne la page du calendrier
-     *
-     * @return string
-     */
-    public function get()
-    {
-        if (!empty($_GET) && $this->isSearch($_GET)) {
-            $this->vue = (int) $_GET['search']['vue'];
-        }
-        if (isset($_GET['search']['groupe'])) {
-            $this->idGroupe = (int) $_GET['search']['groupe'];
-        }
-        if (!empty($_GET['begin'])) {
-            $this->dateDebut = new \DateTimeImmutable($_GET['begin']);
-        } elseif (static::VUE_SEMAINE === $this->vue) {
-            $dateDebut = new \DateTime();
-            $dateDebut->setISODate(date('Y'), date('W'));
-            $dateDebut->setTime(0, 0);
-            $this->dateDebut = new \DateTimeImmutable($dateDebut->format('Y-m-d'));
-        } else {
-            $this->dateDebut = new \DateTimeImmutable(date('Y') . '-' . date('m') . '-01');
-        }
-        if (!empty($_GET['end'])) {
-            $this->dateFin = new \DateTimeImmutable($_GET['end']);
-        } elseif (static::VUE_SEMAINE === $this->vue) {
-            $this->dateFin = $this->dateDebut->modify('+1 week');
-        } else {
-            $this->dateFin = $this->dateDebut->modify('+1 month');
-        }
-        $this->setDatesCourantes();
-
-        /* Div auto fermé par le bottom */
-        $return = '<div id="calendar-wrapper"><h1>' . _('calendrier_titre') . '</h1>';
-        $return .= $this->getFormulaireRecherche();
-        $return .= $this->getCalendrier();
-
-        return $return;
-    }
-
-    /**
-     * Défini les dates de la période courante
-     */
-    private function setDatesCourantes()
-    {
-        if (static::VUE_SEMAINE === $this->vue) {
-            $this->dateDebutMaintenant = new \DateTimeImmutable();
-            $this->dateDebutMaintenant->setISODate(date('Y'), date('W'));
-            $this->dateDebutMaintenant->setTime(0, 0);
-            $this->dateFinMaintenant = $this->dateDebutMaintenant->modify('+1 week');
-        } else {
-            $this->dateDebutMaintenant = new \DateTimeImmutable(date('Y') . '-' . date('m') . '-01');
-            $this->dateFinMaintenant = $this->dateDebutMaintenant->modify('+1 month');
-        }
-    }
-
-    /**
-     * Y a-t-il une recherche dans l'avion ?
-     *
-     * @param array $get
-     *
-     * @return bool
-     */
-    private function isSearch(array $get)
-    {
-        return !empty($get['search']);
-    }
-
-    /**
-     * Retourne le formulaire de recherche
-     *
-     * @return string
-     */
-    private function getFormulaireRecherche()
-    {
-        $form = '<form method="get" action="" class="form-inline search" role="form"><div class="form-group col-md-4 col-sm-5">
-        <label class="control-label col-md-3 col-sm-3" for="vue">Vue&nbsp;:</label>
-        <div class="col-md-8 col-sm-8"><select class="form-control" name="search[vue]" id="vue">';
-
-        foreach ($this->getOptionsVue() as $valeur => $label) {
-            $selected = ($valeur === $this->vue)
-                ? 'selected="selected"'
-                : '';
-            $form .= '<option value="' . $valeur . '" ' . $selected . '>' . _($label) . '</option>';
-        }
-        $form .= '</select></div></div>';
-        if($_SESSION['config']['gestion_groupes']) {
-            $groupesVisiblesUtilisateur = \App\ProtoControllers\Utilisateur::getListeGroupesVisibles($_SESSION['userlogin']);
-            $form .= '<div class="form-group col-md-4 col-sm-5">
-            <label class="control-label col-md-3 col-sm-3" for="groupe">Groupe&nbsp;:</label>
-            <div class="col-md-8 col-sm-8"><select class="form-control" name="search[groupe]" id="groupe">';
-            $form .= '<option value="' . NIL_INT . '">Tous</option>';
-
-            foreach (\App\ProtoControllers\Groupe::getOptions() as $id => $groupe) {
-                if (!in_array($id, $groupesVisiblesUtilisateur)) {
-                    continue;
-                }
-                $selected = ($id ===  $this->idGroupe)
-                    ? 'selected="selected"'
-                    : '';
-                $form .= '<option value="' . $id . '" ' . $selected . '>' . $groupe['nom'] . '</option>';
-            }
-            $form .= '</select></div></div>';
-        }
-
-        $form .= '<div class="form-group"><div class="input-group pull-right">
-        <button type="submit" class="btn btn-default"><i class="fa fa-search" aria-hidden="true"></i></button></div></div>';
-        $form .= '</form>';
-
-        return $form;
-    }
-
-    /**
-     * Retourne les options de select des vues
-     *
-     * @return array
-     */
-    private function getOptionsVue()
-    {
-        return [
-            static::VUE_MOIS => 'vue_mois',
-            static::VUE_SEMAINE => 'vue_semaine',
-        ];
-    }
+    private $moisDemande;
 
     /**
      * Retourne la vue du calendrier
      *
      * @return string
      */
-    private function getCalendrier()
+    public function getCalendrier()
     {
         // TODO: open bar sur les droits :O
-        $groupesVisiblesUserCourant = \App\ProtoControllers\Utilisateur::getListeGroupesVisibles($_SESSION['userlogin']);
-        ddd($groupesVisiblesUserCourant);
-        $utilisateursATrouver = \App\ProtoControllers\Groupe\Utilisateur::getListUtilisateurByGroupeIds($groupesVisiblesUserCourant);
-
-        $injectableCreator = new \App\Libraries\InjectableCreator(\includes\SQL::singleton());
-        $evenements = new \App\Libraries\Calendrier\Facade($injectableCreator);
-        $evenements->fetchEvenements(
-            $this->dateDebut,
-            $this->dateFin,
-            $utilisateursATrouver, $this->canSessionVoirEvenementEnTransit($_SESSION)
-        );
-        $calendar = new \CalendR\Calendar();
         /* Suis pas fan de la répartition par if, mais ça a l'air de faire le job */
-        if (static::VUE_SEMAINE === $this->vue) {
-            $this->setPeriodesSemaine();
-            $return = $this->getNavigation();
-            $return .= $this->getCalendrierSemaine($calendar);
-        } else {
-            $this->setPeriodesMois();
-            $return = $this->getNavigation();
-            $return .= $this->getCalendrierMois($calendar, $evenements);
-        }
-
-        return $return;
-    }
-
-    private function canSessionVoirEvenementEnTransit(array $donnessUtilisateur)
-    {
-        return (isset($donnessUtilisateur['is_resp']) && 'Y' === $donnessUtilisateur['is_resp'])
-            || (isset($donnessUtilisateur['is_rh']) && 'Y' === $donnessUtilisateur['is_rh'])
-            || (isset($donnessUtilisateur['is_admin']) && 'Y' === $donnessUtilisateur['is_admin']);
+        return $this->isVueJournaliere()
+            ? $this->getCalendrierJour()
+            : '';
     }
 
     /**
-     * Défini les périodes utiles pour le calendrier à la semaine
+     * @return bool
      */
-    private function setPeriodesSemaine()
+    private function isVueJournaliere()
     {
-        $this->dateDebutPrecedente = $this->dateDebut->modify('-1 week');
-        $this->dateFin = $this->dateDebut->modify('+1 week');
-        $this->dateFinSuivante = $this->dateFin->modify('+1 week');
+        return $this->jourDemande instanceof \DateTimeInterface;
     }
 
     /**
-     * Défini les périodes utiles pour le calendrier au mois
-     */
-    private function setPeriodesMois()
-    {
-        $this->dateDebutPrecedente = $this->dateDebut->modify('-1 month');
-        $this->dateFinSuivante = $this->dateFin->modify('+1 month');
-    }
-
-    /**
-     * Retourne la vue à la semaine du calendrier
-     *
-     * @param \CalendR\Calendar $calendar
+     * Retourne la vue au jour du calendrier
      *
      * @return string
      */
-    private function getCalendrierSemaine(\CalendR\Calendar $calendar)
+    private function getCalendrierJour()
     {
         /* TODO: La lib ne gère pas les immutables, faire une PR */
-        $week = $calendar->getWeek(new \DateTime($this->dateDebut->format('Y-m-d')));
-        $eventCollection = $calendar->getEvents($week);
+        $week = $this->calendar->getWeek(new \DateTime($this->dateDebut->format('Y-m-d')));
+        $eventCollection = $this->calendar->getEvents($week);
 
-        $return = '<h2>Semaine ' . $week->getBegin()->format('W – Y') . '</h2>';
-        $return .= '<div id="calendrierSemaine" class="calendrier">';
+        $return = '<h2>Jour ' . $week->getBegin()->format('W – Y') . '</h2>';
+        $return .= '<div id="calendrierJour" class="calendrier">';
 
         /* Affichage de l'en-tête */
-        $pweek = $calendar->getWeek(2016, 1);
-        $return .= '<div class="semaine"><div class="minuteId"></div>';
+        $pweek = $this->calendar->getWeek(2016, 1);
+        $return .= '<div class="jour"><div class="minuteId"></div>';
         foreach ($week as $day) {
             $return .= '<div class="en-tete">' . strftime('%a', $day->getBegin()->getTimestamp()) . '</div>';
         }
         $return .= '</div>';
-        $return .= '<div class="semaine">';
+        $return .= '<div class="jour">';
         /* Affichage des événements à la journée du calendrier */
         $inflated = [];
         foreach ($week as $day) {
@@ -365,120 +138,6 @@ final class Calendrier
         }
         $return .= '</div></div>';
         $return .= '</div>';
-
-        return $return;
-    }
-
-    /**
-     * Retourne la vue au mois du calendrier
-     *
-     * @param \CalendR\Calendar $calendar
-     *
-     * @return string
-     */
-    private function getCalendrierMois(\CalendR\Calendar $calendar, \App\Libraries\Calendrier\Facade $evenements)
-    {
-        //ddd($evenements);
-        /* TODO: La lib ne gère pas les immutables, faire une PR */
-        $month = $calendar->getMonth(new \DateTime($this->dateDebut->format('Y-m-d')));
-
-        $return = '<h2>' . strftime('%B %G', $month->getBegin()->getTimestamp()) . '</h2>';
-        $return .= '<div id="calendrierMois" class="calendrier">';
-
-        /* Affichage de l'en-tête */
-        $return .= '<table id="calendrier">';
-        $return .= '<tr id="entete"><th></th>';
-        foreach ($month as $week) {
-            foreach ($week as $day) {
-                $today = ($day->isCurrent()) ? 'today' : '';
-                $jours[] = $day->getBegin()->format('Y-m-d');
-                $return .= '<th class="' . $today . '">' . $day->getBegin()->format('d') . '</th>';
-            }
-        }
-        $return .= '</tr>';
-        foreach ($evenements->getEmployes() as $nom) {
-            $return .= '<tr class="calendrier-employe">';
-            $return .= '<td class="calendrier-nom">' . $nom . '</td>';
-            foreach ($jours as $jour) {
-                $classes = $this->getClassesJour($evenements, $nom, $jour);
-                // TODO: vrai title
-                $return .= '<td class="calendrier-jour ' . $classes . '" title="' . $jour . ' | ' . $nom . ' : <br>ee"><div class="triangle-top"></div><div class="triangle-bottom"></div></td>';
-            }
-            $return .= '</tr>';
-        }
-        $return .= '';
-        $return .= '</tr>';
-        $return .= '</table>';
-
-        /* Affichage des événements du calendrier */
-        /*
-        foreach ($month as $week) {
-            $return .= '<div class="semaine"><div class="semaineId">' . $week . '</div>';
-            foreach ($week as $day) {
-                /* Vérification que le jour est bien dans le mois *
-                $class = (!$month->includes($day)) ? 'horsMois' : '';
-                $today = ($day->isCurrent()) ? 'today' : '';
-                $return .= '<div class="celluleJour ' . $class . '">';
-                $return .= '<div class="jourId ' . $today . '">' . $day->getBegin()->format('j') . '</div>';
-                /* Tous les événements qui sont contenus dans des jours *
-                foreach ($eventCollection->find($day) as $event) {
-                    $title = $event->getTitle();
-                    $avecTitle = (!empty($title)) ? 'evenement-avec-title': '';
-                    $return .= '<div class="' . $avecTitle . ' ' . $event->getClass() . '"
-                    title="' . $title . '"><div class="contenu">' . $event->getName() . '</div>';
-                    /* Un événement qui se termine est forcément avant la fin de la journée *
-                    $return .= ($day->getEnd() < $event->getEnd()) ? '<div class="multijour"></div>' : '';
-                    $return .= '</div>';
-                }
-                $return .= '</div>';
-            }
-            $return .= '</div>';
-        }
-        $return .= '</div>';
-        */
-
-        return $return;
-    }
-
-    private function getClassesJour(\App\Libraries\Calendrier\Facade $evenements, $nom, $jour)
-    {
-        $mois = $this->dateDebut->format('m');
-        $moisJour = date('m', strtotime($jour));
-        if ($mois !== $moisJour) {
-            return 'horsMois';
-        }
-
-        return implode(' ', $evenements->getEvenementsDate($nom, $jour));
-    }
-
-    /**
-     * Retourne la navigation
-     *
-     * @return string
-     */
-    private function getNavigation()
-    {
-        $urlCalendrier = ROOT_PATH . 'calendrier.php';
-        $queryBase = [
-            'search[vue]' => $this->vue,
-            'search[groupe]' => $this->idGroupe,
-        ];
-        $queryPrec = [
-            'begin' => $this->dateDebutPrecedente->format('Y-m-d'),
-            'end'   => $this->dateDebut->format('Y-m-d'),
-        ];
-        $queryCurrent = [
-            'begin' => $this->dateDebutMaintenant->format('Y-m-d'),
-            'end'   => $this->dateFinMaintenant->format('Y-m-d'),
-        ];
-        $querySuc = [
-            'begin' => $this->dateFin->format('Y-m-d'),
-            'end'   => $this->dateFinSuivante->format('Y-m-d'),
-        ];
-
-        $return = '<div class="btn-group pull-right"><a class="btn btn-default" href="' . $urlCalendrier . '?' . http_build_query($queryBase + $queryPrec) . '"><i class="fa fa-chevron-left" aria-hidden="true"></i></a>';
-        $return .= '<a class="btn btn-default" title="' . _('retour_periode_courante') . '" href="' . $urlCalendrier . '?' . http_build_query($queryBase + $queryCurrent) . '"><i class="fa fa-home" aria-hidden="true"></i></a>';
-        $return .= '<a class="btn btn-default" href="' . $urlCalendrier . '?' . http_build_query($queryBase + $querySuc) . '"><i class="fa fa-chevron-right" aria-hidden="true"></i></a></div>';
 
         return $return;
     }
--- App/Views/Calendrier.php
+++ App/Views/Calendrier.php
@@ -1,29 +1,78 @@
 <?php
-$noms = ['Abe', 'Lafayette', 'Georges', 'Stan', 'Barry'];
-$debut = new \DateTime(date('Y-m-01'));
-$month = $calendar->getMonth($debut);
+/*
+ * Variables disponibles :
+ * $calendar
+ * $session
+ * $calendrier
+ * $evenements
+ */
+$mois = $calendar->getMonth(new \DateTime($moisDemande->format('Y-m-d')));
 $jours = [];
+$moisPrecedent = getUrlMois($moisDemande->modify('-1 month'), $session, $idGroupe);
+$moisCourant = getUrlMois(new \DateTimeImmutable(), $session, $idGroupe);
+$moisSuivant = getUrlMois($moisDemande->modify('+1 month'), $session, $idGroupe);
 ?>
 
-<table id="calendrier">
-    <tr id="entete">
-        <th></th>
-        <?php foreach ($month as $week) : ?>
-            <?php foreach ($week as $day) : ?>
+<?php /* Div auto fermé par le bottom */ ?>
+<div id="calendar-wrapper"><h1><?= _('calendrier_titre') ?></h1>
+
+<form method="get" action="" class="form-inline search" role="form">
+    <div class="form-group col-md-4 col-sm-5">
+        <label class="control-label col-md-3 col-sm-3" for="groupe">Groupe&nbsp;:</label>
+        <div class="col-md-8 col-sm-8">
+            <select class="form-control" name="groupe" id="groupe">
+                <option value="<?= NIL_INT ?>">Tous</option>
                 <?php
-                $today = ($day->isCurrent()) ? 'today' : '';
-                $jours[] = $day->getBegin()->format('Y-m-d');
-                ?>
-                <th class="<?= $today ?>"><?= $day->getBegin()->format('j') ?></th>
+                    foreach (\App\ProtoControllers\Groupe::getOptions() as $id => $groupe) {
+                        $selected = ($id === $idGroupe)
+                        ? 'selected="selected"'
+                        : '';
+                        echo '<option value="' . $id . '" ' . $selected . '>' . $groupe['nom'] . '</option>';
+                    }
+                    ?>
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="input-group pull-right"><button type="submit" class="btn btn-default"><i class="fa fa-search" aria-hidden="true"></i></button>
+        </div>
+    </div>
+    <input type="hidden" name="session" value="<?= $session ?>" />
+</form>
+
+<div class="btn-group pull-right">
+    <a class="btn btn-default" href="<?= $moisPrecedent ?>"><i class="fa fa-chevron-left" aria-hidden="true"></i></a>
+    <a class="btn btn-default" title="<?= _('retour_periode_courante') ?>" href="<?= $moisCourant ?>"><i class="fa fa-home" aria-hidden="true"></i></a>
+    <a class="btn btn-default" href="<?= $moisSuivant ?>"><i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+</div>
+
+<h2><?= strftime('%B %G', $mois->getBegin()->getTimestamp()) ?></h2>
+<div id="calendrierMois" class="calendrier">
+    <table id="calendrier">
+        <tr id="entete"><th></th>
+            <?php foreach ($mois as $week) : ?>
+            <?php foreach ($week as $day) : ?>
+            <?php
+            $today = ($day->isCurrent()) ? 'today' : '';
+            $jourString = $day->getBegin()->format('Y-m-d');
+            $jours[] = $jourString;
+            ?>
+            <th class="<?= $today ?>">
+                <a href=?session=<?= $session ?>&jour=<?= $jourString ?>><?= $day->getBegin()->format('d') ?></a>
+            </th>
             <?php endforeach ?>
-        <?php endforeach ?>
-    </tr>
-    <?php foreach ($noms as $nom) : ?>
+            <?php endforeach ?>
+        </tr>
+        <?php foreach ($evenements->getEmployes() as $nom) : ?>
         <tr class="calendrier-employe">
             <td class="calendrier-nom"><?= $nom ?></td>
-        <?php foreach ($jours as $jour) : ?>
-            <td class="calendrier-jour" title="<?= $jour ?>"></td>
-        <?php endforeach ?>
+            <?php foreach ($jours as $jour) : ?>
+            <td class="calendrier-jour <?= getClassesJour($evenements, $nom, $jour, $moisDemande) ?>" title=" | ">
+                <div class="triangle-top"></div>
+                <div class="triangle-bottom"></div>
+            </td>
+            <?php endforeach ?>
         </tr>
-    <?php endforeach ?>
-</table>
+        <?php endforeach ?>
+    </table>
+</div>
