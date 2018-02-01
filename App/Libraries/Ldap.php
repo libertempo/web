12b8e87cdf22c4beaeab202aafeb1e516b6b1aba
wouldsmina <wouldsmina@tuxfamily.org>
2018-02-01 22:35:49 +0100

    test unitaire

--- App/Libraries/Ldap.php
+++ App/Libraries/Ldap.php
@@ -12,42 +12,30 @@ namespace App\Libraries;
  */
 class Ldap
 {
-    private $ldapConn;
-    private $server;
-    private $version;
-    private $bindUser;
-    private $bindPassword;
-    private $searchdn;
-    private $attrPrenom;
-    private $attrNom;
-    private $attrMail;
-    private $attrLogin;
-    private $attrNomAff;
-    private $attrFiltre;
-    private $filtre;
+    private $configuration = [];
 
     public function __construct($confLdap) {
-        $this->server = $confLdap['server'];
-        $this->version = $confLdap['version'];
-        $this->bindUser = "" == $confLdap['bindUser'] ? null : $confLdap['bindUser'];
-        $this->bindPassword = "" == $confLdap['bindPassword'] ? null : $confLdap['bindPassword'];
-        $this->searchdn = $confLdap['server'];
-        $this->attrNom = $confLdap['attrNom'];
-        $this->attrPrenom = $confLdap['attrPrenom'];
-        $this->attrLogin = $confLdap['attrLogin'];
-        $this->attrNomAff = $confLdap['attrNomAff'];
-        $this->attrMail = $confLdap['attrMail'];
-        $this->attrFiltre = $confLdap['attrFiltre'];
-        $this->filtre = $confLdap['filtre'];
+        $this->configuration['server'] = $confLdap['server'];
+        $this->configuration['version'] = $confLdap['version'];
+        $this->configuration['bindUser'] = "" == $confLdap['bindUser'] ? null : $confLdap['bindUser'];
+        $this->configuration['bindPassword'] = "" == $confLdap['bindPassword'] ? null : $confLdap['bindPassword'];
+        $this->configuration['searchdn'] = $confLdap['server'];
+        $this->configuration['attrNom'] = $confLdap['attrNom'];
+        $this->configuration['attrPrenom'] = $confLdap['attrPrenom'];
+        $this->configuration['attrLogin'] = $confLdap['attrLogin'];
+        $this->configuration['attrNomAff'] = $confLdap['attrNomAff'];
+        $this->configuration['attrMail'] = $confLdap['attrMail'];
+        $this->configuration['attrFiltre'] = $confLdap['attrFiltre'];
+        $this->configuration['filtre'] = $confLdap['filtre'];
 
-        $this->ldapConn = \ldap_connect($this->server);
+        $this->configuration['ldapConn'] = \ldap_connect($this->configuration['server']);
 
-        if ($this->version != 0) {
-            ldap_set_option($this->ldapConn, LDAP_OPT_PROTOCOL_VERSION, $this->version);
-            ldap_set_option($this->ldapConn, LDAP_OPT_REFERRALS, 0);
+        if ($this->configuration['version'] != 0) {
+            ldap_set_option($this->configuration['ldapConn'], LDAP_OPT_PROTOCOL_VERSION, $this->configuration['version']);
+            ldap_set_option($this->configuration['ldapConn'], LDAP_OPT_REFERRALS, 0);
         }
 
-        if (!ldap_bind($this->ldapConn, $this->bindUser, $this->bindPassword)) {
+        if (!ldap_bind($this->configuration['ldapConn'], $this->configuration['bindUser'], $this->configuration['bindPassword'])) {
           throw new \Exception(_('Erreur ldap'));
         }
     }
@@ -61,38 +49,36 @@ class Ldap
     private function getInfosUser($nom)
     {
         $data = [];
-        $filter = "(&(" . $this->attrNomAff . "=" . $nom . "*)
-                    (" . $this->attrFiltre . "=" . $this->filtre . "))";
+        $filter = "(&(" . $this->configuration['attrNomAff'] . "=" . $nom . "*)
+                    (" . $this->configuration['attrFiltre'] . "=" . $this->configuration['filtre'] . "))";
 
-        $attributs = [$this->attrLogin, $this->attrNom, $this->attrPrenom];
+        $attributs = [$this->configuration['attrLogin'], $this->configuration['attrNom'], $this->configuration['attrPrenom']];
         
-        $searchResult = ldap_search($this->ldapConn, $this->searchdn, $filter, $attributs, 0, 10);
-        $entries = ldap_get_entries($this->ldapConn,$searchResult);
+        $searchResult = ldap_search($this->configuration['ldapConn'], $this->configuration['searchdn'], $filter, $attributs, 0, 10);
+        $entries = ldap_get_entries($this->configuration['ldapConn'],$searchResult);
 
-        if (0 < $entries['count']) {
-            for ($i=0; $i<$entries["count"]; $i++) {
-                $data[] = [
-                    'login' => $entries[$i][$this->attrLogin][0],
-                    'nom' => $entries[$i][$this->attrNom][0],
-                    'prenom' => $entries[$i][$this->attrPrenom][0],
-                    ];
-            }
+        for ($i=0; $i<$entries["count"]; $i++) {
+            $data[] = [
+                'login' => $entries[$i][$this->configuration['attrLogin']][0],
+                'nom' => $entries[$i][$this->configuration['attrNom']][0],
+                'prenom' => $entries[$i][$this->configuration['attrPrenom']][0],
+                ];
         }
         return $data;
     }
 
     public function getEmailUser($login)
     {
-        $filter = "(&(" . $this->attrLogin . "=" . $login . ")
-                    (" . $this->attrFiltre . "=" . $this->filtre . "))";
+        $filter = "(&(" . $this->configuration['attrLogin'] . "=" . $login . ")
+                    (" . $this->configuration['attrFiltre'] . "=" . $this->configuration['filtre'] . "))";
 
-        $attributs = [$this->attrLogin, $this->attrMail];
+        $attributs = [$this->configuration['attrLogin'], $this->configuration['attrMail']];
         
-        $searchResult = ldap_search($this->ldapConn, $this->searchdn, $filter, $attributs, 0, 1);
-        $entries = ldap_get_entries($this->ldapConn,$searchResult);
+        $searchResult = ldap_search($this->configuration['ldapConn'], $this->configuration['searchdn'], $filter, $attributs, 0, 1);
+        $entries = ldap_get_entries($this->configuration['ldapConn'],$searchResult);
 
         if (0 < $entries['count']) {
-            return $entries[0][$this->attrMail][0];
+            return $entries[0][$this->configuration['attrMail']][0];
         }
 
         return "";
---
+++ Tests/Units/App/Libraries/Ldap.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Units\App\Libraries;
+
+class Ldap extends \Tests\Units\TestUnit {
+
+    public function beforeTestMethod($method)
+    {
+        parent::beforeTestMethod($method);
+
+        $this->function->ldap_connect = false;
+    }
+
+    // là tu as ton premier cas d'échec, quand la construction se vautre
+    public function testConstructFailed()
+    {
+        $this->function->ldap_bind = false;
+        $this->exception(function() {
+            $this->newTestedInstance();
+        });
+    }
+
+    public function testGetMailUser()
+    {
+        $this->function->ldap_bind = true;
+        $this->function->ldap_search = false;
+        $this->function->ldap_get_entries = [
+            'count' => 1,
+            0 => ['mail' => [0 => 'mail@example.org'],
+            ],
+        ];
+        $this->newTestedInstance($this->configuration);
+        $this->string($this->testedInstance->getEmailUser('toto'))->isEqualTo('mail@example.org');
+    }
+
+    public function testGetInfosUser()
+    {
+        $this->function->ldap_bind = true;
+        $this->function->ldap_search = false;
+        $this->function->ldap_get_entries = [
+            'count' => 2,
+            0 => ['uid' => [0 => 'login'],
+                'sn' => [0 => 'nom'],
+                'givenname' => [0 => 'prenom'],
+            ],
+            1 => ['uid' => [0 => 'login2'],
+                'sn' => [0 => 'nom2'],
+                'givenname' => [0 => 'prenom2'],
+            ],
+        ];
+        $return = '[{"login":"login","nom":"nom","prenom":"prenom"},{"login":"login2","nom":"nom2","prenom":"prenom2"}]';
+        $this->newTestedInstance($this->configuration);
+        $this->string($this->testedInstance->searchLdap('toto'))->isEqualTo($return);
+    }
+
+    public function testConstructSuccess()
+    {
+        $this->function->ldap_bind = true;
+        $this->newTestedInstance($this->configuration);
+        $this->object($this->testedInstance)->isInstanceOf(\App\Libraries\Ldap::class);
+    }
+
+    private $configuration = [
+        'server' => 'server',
+        'version' => 3,
+        'attrNom' => 'sn',
+        'attrPrenom' => 'givenname',
+        'attrLogin' => 'uid',
+        'attrNomAff' => 'nomAffichee',
+        'attrMail' => 'mail',
+        'attrFiltre' => 'attrFiltre',
+        'filtre' => 'filtre',
+        'bindUser' => 'user',
+        'bindPassword' => 'pwd'
+    ];
+
+}
