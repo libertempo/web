c0c29b4d9010a4542f0b8126e6466b8abe5a4699
Prytoegrian <5312739+prytoegrian@users.noreply.github.com>
2018-08-11 19:52:43 +0200

    Correction bug sauvegarde (#689)

--- admin/admin_index.php
+++ admin/admin_index.php
@@ -1,7 +1,6 @@
 <?php
-define('ROOT_PATH', '../');
-define('INCLUDE_PATH',     ROOT_PATH . 'includes/');
-require_once INCLUDE_PATH . 'define.php';
+defined('ROOT_PATH') or define('ROOT_PATH', '../');
+defined('INCLUDE_PATH') or define('INCLUDE_PATH',     ROOT_PATH . 'includes/');
 include_once INCLUDE_PATH . 'session.php';
 
 // verif des droits du user à afficher la page
@@ -11,7 +10,7 @@ $config = new \App\Libraries\Configuration(\includes\SQL::singleton());
 /*************************************/
 // recup des parametres reçus :
 // SERVER
-$PHP_SELF = filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_URL);
+$PHP_SELF = filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_URL);
 
 /*********************************/
 /*   COMPOSITION DU HEADER...    */
@@ -32,7 +31,7 @@ $tab_type_cong=recup_tableau_types_conges();
 // recup du tableau des types de conges exceptionnels (seulement les conges exceptionnels)
 $tab_type_conges_exceptionnels=recup_tableau_types_conges_exceptionnels();
 
-echo '<div main-content">';
+echo '<div class="main-content">';
     include_once ROOT_PATH . 'admin/admin_db_sauve.php';
 echo '</div>';
 
