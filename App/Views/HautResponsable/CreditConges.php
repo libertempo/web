f162882f5401d0013684ebca416e2dc897f9cbc0
Prytoegrian <prytoegrian@protonmail.com>
2018-08-30 09:03:25 +0200

    Création template de base

--- App/Views/HautResponsable/CreditConges.php
+++ App/Views/HautResponsable/CreditConges.php
@@ -1,10 +1,141 @@
 <?php
 /**
  * $titre
- * $return
+ * $tab_all_users_du_hr
+ * $tab_all_users_du_grand_resp
+ * $list_group
  */
 ?>
 
 <h1><?= $titre ?></h1>
+<?php if (empty($tab_all_users_du_hr) && empty($tab_all_users_du_grand_resp)) : ?>
+    <?= _('resp_etat_aucun_user') ?><br>
+<?php else : ?>
+    <h2><?= _('resp_ajout_conges_ajout_all') ?></h2>
+    <form action="<?= $PHP_SELF ?>?onglet=ajout_conges" method="POST">
+        <fieldset class="cal_saisie">
+            <div class="table-responsive">
+                <table class="table table-hover table-condensed table-striped">
+                    <thead>
+                        <tr>
+                            <th colspan="2"><?= _('resp_ajout_conges_nb_jours_all_1') ?> <?= _('resp_ajout_conges_nb_jours_all_2') ?></th>
+                            <th><?= _('resp_ajout_conges_calcul_prop') ?></th>
+                            <th><?= _('divers_comment_maj_1') ?></th>
+                        </tr>
+                    </thead>
+                <?php foreach($tab_type_cong as $id_conges => $libelle) : ?>
+                    <tr>
+                        <td><strong><?= $libelle ?><strong></td>
+                        <td><input class="form-control" type="text" name="tab_new_nb_conges_all[<?= $id_conges ?>]" size="6" maxlength="6" value="0"></td>
+                        <td><?= _('resp_ajout_conges_oui') ?>
+                            <input type="checkbox" name="tab_calcul_proportionnel[<?= $id_conges ?>]" value="TRUE" checked>
+                        </td>
+                        <td><input class="form-control" type="text" name="tab_new_comment_all[<?= $id_conges ?>]" size="30" maxlength="200" value=""></td>
+                    </tr>
+                <?php endforeach ; ?>
+                </table>
+            </div>
+            <p><?= _('resp_ajout_conges_calcul_prop_arondi') ?> !</p>
+            <input class="btn" type="submit" value="<?= _('form_valid_global') ?>">
+        </fieldset>
+        <input type="hidden" name="ajout_global" value="TRUE">
+    </form>
+    <br>
+    <?php if (!empty($list_group)) : ?>
+    <h2><?= _('resp_ajout_conges_ajout_groupe') ?></h2>
+    <form action="<?= $PHP_SELF ?>?onglet=ajout_conges" method="POST">
+        <fieldset class="cal_saisie">
+            <div class="table-responsive">
+                <table class="table table-hover table-condensed table-striped">
+                    <tr>
+                        <td class="big"><?= _('resp_ajout_conges_choix_groupe') ?> : </td>
+                        <td colspan="3">
+                            <select name="choix_groupe">
+                            <?php foreach ($groupes as $groupe) : ?>
+                                <option value="<?= $group['g_gid'] ?>"><?= $group['g_groupename'] ?></option>
+                            <?php endforeach ; ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th colspan="2"><?= _('resp_ajout_conges_nb_jours_all_1') ?> <?= _('resp_ajout_conges_nb_jours_all_2') ?></th>
+                        <th><?= _('resp_ajout_conges_calcul_prop') ?></th>
+                        <th><?= _('divers_comment_maj_1') ?></th>
+                    </tr>
+                <?php foreach($tab_type_cong as $id_conges => $libelle) : ?>
+                    <tr>
+                        <td><strong><?= $libelle ?><strong></td>
+                        <td><input class="form-control" type="text" name="tab_new_nb_conges_all[<?= $id_conges ?>]" size="6" maxlength="6" value="0"></td>
+                        <td><?= _('resp_ajout_conges_oui') ?>
+                            <input type="checkbox" name="tab_calcul_proportionnel[<?= $id_conges ?>]" value="TRUE" checked>
+                        </td>
+                        <td>
+                            <input class="form-control" type="text" name="tab_new_comment_all[<?= $id_conges ?>" size="30" maxlength="200" value="">
+                        </td>
+                    </tr>
+                <?php endforeach ; ?>
+                </table>
+            </div>
+            <p><?= _('resp_ajout_conges_calcul_prop_arondi') ?> ! </p>
+            <input class="btn" type="submit" value="<?= _('form_valid_groupe') ?>">
+        </fieldset>
+        <input type="hidden" name="ajout_groupe" value="TRUE">
+    </form>
+    <br>
+    <? endif ; ?>
 
-<?= $return ?>
+    <h2>Ajout par utilisateur</h2>
+    <form action="<?= $PHP_SELF ?>?onglet=ajout_conges" method="POST">
+        <div class="table-responsive">
+            <table class="table table-hover table-condensed table-striped">
+                <thead>
+                    <tr align="center">
+                        <th><?= _('divers_nom_maj_1') ?></th>
+                        <th><?= _('divers_prenom_maj_1') ?></th>
+                        <th><?= _('divers_quotite_maj_1') ?></th>
+                    <?php foreach($tab_type_cong as $libelle) : ?>
+                        <th><?= $libelle ?><br><i>(<?= _('divers_solde') ?>)</i></th>
+                        <th><?= $libelle ?><br><?= _('resp_ajout_conges_nb_jours_ajout') ?></th>
+                    <?php endforeach ; ?>
+                    <?php foreach($tab_type_conges_exceptionnels as $libelle) : ?>
+                        <th><?= $libelle ?><br><i>(<?= _('divers_solde') ?>)</i></th>
+                        <th><?= $libelle ?><br><?= _('resp_ajout_conges_nb_jours_ajout') ?></th>
+                    <?php endforeach ; ?>
+                        <th><?= _('divers_comment_maj_1') ?><br></th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php $i = true; ?>
+                <?php foreach ($tab_all_users_du_hr as $current_login => $tab_current_user) : ?>
+                    <?php if ($tab_current_user['is_active'] != "Y") : ?>
+                        <?php continue; ?>
+                    <?php endif; ?>
+                    <tr class="<?= ($i ? 'i' : 'p') ?>">
+                        <?php $tab_conges = $tab_current_user['conges']; ?>
+                        <td><?= $tab_current_user['nom'] ?></td>
+                        <td><?= $tab_current_user['prenom'] ?></td>
+                        <td><?= $tab_current_user['quotite'] ?>%</td>
+                    <?php foreach($tab_type_cong as $id_conges => $libelle) : ?>
+                        <td><?= $tab_conges[$libelle]['nb_an'] ?> <i>(<?= $tab_conges[$libelle]['solde'] ?>)</i></td>
+                        <td align="center" class="histo">
+                            <input class="form-control" type="text" name="tab_champ_saisie[<?= $current_login ?>][<?= $id_conges ?>]" size="6" maxlength="6" value="0">
+                        </td>
+                    <?php endforeach ; ?>
+                    <?php foreach($tab_type_conges_exceptionnels as $id_conges => $libelle) : ?>
+                        <td><i>(<?= $tab_conges[$libelle]['solde'] ?>)</i></td>
+                        <td align="center" class="histo">
+                            <input class="form-control" type="text" name="tab_champ_saisie[<?= $current_login ?>][ <?= $id_conges ?>]" size="6" maxlength="6" value="0">
+                        </td>
+                    <?php endforeach ; ?>
+                        <td align="center" class="histo">
+                            <input class="form-control" type="text" name="tab_commentaire_saisie[<?= $current_login ?>]" size="30" maxlength="200" value="">
+                        </td>
+                    </tr>
+                    <?php $i = !$i; ?>
+                <?php endforeach ; ?>
+                </tbody>
+            </table>
+        <input type="hidden" name="ajout_conges" value="TRUE">
+        <input class="btn" type="submit" value="<?= _('form_submit') ?>">
+    </form>
+    <br>
+<? endif ; ?>
--- hr/hr_ajout_conges.php
+++ hr/hr_ajout_conges.php
@@ -151,42 +151,6 @@ function ajout_global_groupe($choix_groupe, $tab_new_nb_conges_all, $tab_calcul_
     }
 }
 
-function affichage_saisie_globale_pour_tous($tab_type_conges) : string
-{
-    $PHP_SELF = filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_URL);
-    $return = '';
-
-    /************************************************************/
-    /* SAISIE GLOBALE pour tous les utilisateurs du responsable */
-    $return .= '<h2>' . _('resp_ajout_conges_ajout_all') . '</h2>';
-    $return .= '<form action="' . $PHP_SELF . '?onglet=ajout_conges" method="POST">';
-    $return .= '<fieldset class="cal_saisie">';
-    $return .= '<div class="table-responsive"><table class="table table-hover table-condensed table-striped">';
-    $return .= '<thead>';
-    $return .= '<tr>';
-    $return .= '<th colspan="2">' . _('resp_ajout_conges_nb_jours_all_1') . ' ' . _('resp_ajout_conges_nb_jours_all_2') . '</th>';
-    $return .= '<th>' . _('resp_ajout_conges_calcul_prop') . '</th>';
-    $return .= '<th>' . _('divers_comment_maj_1') . '</th>';
-    $return .= '</tr>';
-    $return .= '</thead>';
-    foreach($tab_type_conges as $id_conges => $libelle) {
-        $return .= '<tr>';
-        $return .= '<td><strong>' . $libelle . '<strong></td>';
-        $return .= '<td><input class="form-control" type="text" name="tab_new_nb_conges_all[' . $id_conges . ']" size="6" maxlength="6" value="0"></td>';
-        $return .= '<td>' . _('resp_ajout_conges_oui') . '<input type="checkbox" name="tab_calcul_proportionnel[' . $id_conges . ']" value="TRUE" checked></td>';
-        $return .= '<td><input class="form-control" type="text" name="tab_new_comment_all[' . $id_conges . ']" size="30" maxlength="200" value=""></td>';
-        $return .= '</tr>';
-    }
-    $return .= '</table></div>';
-    // texte sur l'arrondi du calcul proportionnel
-    $return .= '<p>' . _('resp_ajout_conges_calcul_prop_arondi') . '!</p>';
-    // bouton valider
-    $return .= '<input class="btn" type="submit" value="' . _('form_valid_global') . '">';
-    $return .= '</fieldset>';
-    $return .= '<input type="hidden" name="ajout_global" value="TRUE">';
-    $return .= '</form>';
-    return $return;
-}
 
 // recup de la liste de tous les groupes pour le mode RH
 function get_list_groupes_pour_rh($user_login) : string
@@ -208,145 +172,6 @@ function get_list_groupes_pour_rh($user_login) : string
     return $list_group;
 }
 
-function affichage_saisie_globale_groupe($tab_type_conges) : string
-{
-    $PHP_SELF = filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_URL);
-    $return = '';
-
-    /***********************************************************************/
-    /* SAISIE GROUPE pour tous les utilisateurs */
-
-    // on établi la liste complète des groupes pour le mode RH
-    $list_group = get_list_groupes_pour_rh($_SESSION['userlogin']);
-
-    if ($list_group!="") //si la liste n'est pas vide ( serait le cas si n'est responsable d'aucun groupe)
-    {
-        $return .= '<h2>' . _('resp_ajout_conges_ajout_groupe') . '</h2>';
-        $return .= '<form action="' . $PHP_SELF . '?onglet=ajout_conges" method="POST">';
-        $return .= '<fieldset class="cal_saisie">';
-        $return .= '<div class="table-responsive"><table class="table table-hover table-condensed table-striped">';
-        $return .= '<tr>';
-        $return .= '<td class="big">' . _('resp_ajout_conges_choix_groupe') . ' : </td>';
-        // création du select pour le choix du groupe
-        $text_choix_group="<select name=\"choix_groupe\" >";
-        $sql_group = "SELECT g_gid, g_groupename FROM conges_groupe WHERE g_gid IN ($list_group) ORDER BY g_groupename "  ;
-        $ReqLog_group = \includes\SQL::singleton()->query($sql_group) ;
-
-        while ($resultat_group = $ReqLog_group->fetch_array()) {
-            $current_group_id=$resultat_group["g_gid"];
-            $current_group_name=$resultat_group["g_groupename"];
-            $text_choix_group=$text_choix_group."<option value=\"$current_group_id\" >$current_group_name</option>";
-        }
-        $text_choix_group=$text_choix_group."</select>" ;
-
-        $return .= '<td colspan="3">' . $text_choix_group . '</td>';
-        $return .= '</tr>';
-        $return .= '<tr>';
-        $return .= '<th colspan="2">' . _('resp_ajout_conges_nb_jours_all_1') . ' ' . _('resp_ajout_conges_nb_jours_all_2') . '</th>';
-        $return .= '<th>' ._('resp_ajout_conges_calcul_prop') . '</th>';
-        $return .= '<th>' . _('divers_comment_maj_1') . '</th>';
-        $return .= '</tr>';
-        foreach($tab_type_conges as $id_conges => $libelle) {
-            $return .= '<tr>';
-            $return .= '<td><strong>' . $libelle . '<strong></td>';
-            $return .= '<td><input class="form-control" type="text" name="tab_new_nb_conges_all[' . $id_conges . ']" size="6" maxlength="6" value="0"></td>';
-            $return .= '<td>' . _('resp_ajout_conges_oui') . '<input type="checkbox" name="tab_calcul_proportionnel[' . $id_conges . ']" value="TRUE" checked></td>';
-            $return .= '<td><input class="form-control" type="text" name="tab_new_comment_all[' . $id_conges . ']" size="30" maxlength="200" value=""></td>';
-            $return .= '</tr>';
-        }
-        $return .= '</table></div>';
-        $return .= '<p>' . _('resp_ajout_conges_calcul_prop_arondi') . '! </p>';
-        $return .= '<input class="btn" type="submit" value="' . _('form_valid_groupe') . '">';
-        $return .= '</fieldset>';
-        $return .= '<input type="hidden" name="ajout_groupe" value="TRUE">';
-        $return .= '</form>';
-    }
-    return $return;
-}
-
-function affichage_saisie_user_par_user($tab_type_conges, $tab_type_conges_exceptionnels, $tab_all_users_du_hr, $tab_all_users_du_grand_resp) : string
-{
-    $config = new \App\Libraries\Configuration(\includes\SQL::singleton());
-    $PHP_SELF = filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_URL);
-    $return = '';
-
-    /************************************************************/
-    /* SAISIE USER PAR USER pour tous les utilisateurs du responsable */
-    $return .= '<h2>Ajout par utilisateur</h2>';
-    $return .= '<form action="' . $PHP_SELF . '?onglet=ajout_conges" method="POST">';
-
-    if ( (count($tab_all_users_du_hr)!=0) || (count($tab_all_users_du_grand_resp)!=0) ) {
-        // AFFICHAGE TITRES TABLEAU
-        $return .= '<div class="table-responsive"><table class="table table-hover table-condensed table-striped">';
-        $return .= '<thead>';
-        $return .= '<tr align="center">';
-        $return .= '<th>' . _('divers_nom_maj_1') . '</th>';
-        $return .= '<th>' . _('divers_prenom_maj_1') . '</th>';
-        $return .= '<th>' . _('divers_quotite_maj_1') . '</th>';
-        foreach($tab_type_conges as $id_conges => $libelle) {
-            $return .= '<th>' . $libelle . '<br><i>(' . _('divers_solde') . ')</i></th>';
-            $return .= '<th>' . $libelle . '<br>' . _('resp_ajout_conges_nb_jours_ajout') . '</th>';
-        }
-        if ($config->isCongesExceptionnelsActive()) {
-            foreach($tab_type_conges_exceptionnels as $id_conges => $libelle) {
-                $return .= '<th>' . $libelle . '<br><i>(' . _('divers_solde') . ')</i></th>';
-                $return .= '<th>' . $libelle . '<br>' . _('resp_ajout_conges_nb_jours_ajout') . '</th>';
-            }
-        }
-        $return .= '<th>'. _('divers_comment_maj_1') . '<br></th>';
-        $return .= '</tr>';
-        $return .= '</thead>';
-        $return .= '<tbody>';
-
-        // AFFICHAGE LIGNES TABLEAU
-        $cpt_lignes=0 ;
-        $tab_champ_saisie_conges=array();
-
-        $i = true;
-        asort($tab_all_users_du_hr);
-        // affichage des users dont on est responsable :
-        foreach($tab_all_users_du_hr as $current_login => $tab_current_user) {
-            if ($tab_current_user['is_active'] == "Y") {
-                $return .= '<tr class="' . ($i ? 'i' : 'p') . '">';
-                //tableau de tableaux les nb et soldes de conges d'un user (indicé par id de conges)
-                $tab_conges=$tab_current_user['conges'];
-
-                /** sur la ligne ,   **/
-                $return .= '<td>' . $tab_current_user['nom'] . '</td>';
-                $return .= '<td>' . $tab_current_user['prenom'] . '</td>';
-                $return .= '<td>' . $tab_current_user['quotite'] . '%</td>';
-
-                foreach($tab_type_conges as $id_conges => $libelle) {
-                    /** le champ de saisie est <input type="text" name="tab_champ_saisie[valeur de u_login][id_du_type_de_conges]" value="[valeur du nb de jours ajouté saisi]"> */
-                    $champ_saisie_conges="<input class=\"form-control\" type=\"text\" name=\"tab_champ_saisie[$current_login][$id_conges]\" size=\"6\" maxlength=\"6\" value=\"0\">";
-                    $return .= '<td>' . $tab_conges[$libelle]['nb_an'] . ' <i>(' . $tab_conges[$libelle]['solde'] . ')</i></td>';
-                    $return .= '<td align="center" class="histo">' . $champ_saisie_conges . '</td>';
-                }
-                if ($config->isCongesExceptionnelsActive()) {
-                    foreach($tab_type_conges_exceptionnels as $id_conges => $libelle) {
-                        /** le champ de saisie est <input type="text" name="tab_champ_saisie[valeur de u_login][id_du_type_de_conges]" value="[valeur du nb de jours ajouté saisi]"> */
-                        $champ_saisie_conges="<input class=\"form-control\" type=\"text\" name=\"tab_champ_saisie[$current_login][$id_conges]\" size=\"6\" maxlength=\"6\" value=\"0\">";
-                        $return .= '<td><i>(' . $tab_conges[$libelle]['solde'] . ')</i></td>';
-                        $return .= '<td align="center" class="histo">' . $champ_saisie_conges . '</td>';
-                    }
-                }
-                $return .= '<td align="center" class="histo"><input class="form-control" type="text" name="tab_commentaire_saisie[' . $current_login . ']" size="30" maxlength="200" value=""></td>';
-                $return .= '</tr>';
-                $cpt_lignes++ ;
-                $i = !$i;
-            }
-        }
-
-        $return .= '</tbody>';
-        $return .= '</table>';
-
-        $return .= '<input type="hidden" name="ajout_conges" value="TRUE">';
-        $return .= '<input class="btn" type="submit" value="' . _('form_submit') . '">';
-        $return .= ' </form>';
-    }
-
-    return $return;
-}
 
 //var pour resp_ajout_conges_all.php
 $ajout_conges = getpost_variable('ajout_conges');
@@ -362,7 +187,7 @@ if ('true' === $ajout_conges) {
     $tab_commentaire_saisie        = getpost_variable('tab_commentaire_saisie');
 
     ajout_conges($tab_champ_saisie, $tab_commentaire_saisie);
-    redirect( ROOT_PATH .'hr/hr_index.php');
+    redirect( ROOT_PATH . 'hr/hr_index.php');
 }
 
 if ('true' === $ajout_global) {
@@ -372,7 +197,7 @@ if ('true' === $ajout_global) {
     $tab_new_comment_all         = getpost_variable('tab_new_comment_all');
 
     ajout_global($tab_new_nb_conges_all, $tab_calcul_proportionnel, $tab_new_comment_all);
-    redirect( ROOT_PATH .'hr/hr_index.php');
+    redirect( ROOT_PATH . 'hr/hr_index.php');
 }
 
 if ('true' === $ajout_groupe) {
@@ -383,12 +208,11 @@ if ('true' === $ajout_groupe) {
     $choix_groupe                = getpost_variable('choix_groupe');
 
     ajout_global_groupe($choix_groupe, $tab_new_nb_conges_all, $tab_calcul_proportionnel, $tab_new_comment_all);
-    redirect( ROOT_PATH .'hr/hr_index.php');
+    redirect( ROOT_PATH . 'hr/hr_index.php');
 }
 
 $config = new \App\Libraries\Configuration(\includes\SQL::singleton());
 $PHP_SELF = filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_URL);
-$return = '';
 
 // recup du tableau des types de conges (seulement les congesexceptionnels )
 if ($config->isCongesExceptionnelsActive()) {
@@ -401,25 +225,17 @@ if ($config->isCongesExceptionnelsActive()) {
 // (prend en compte le resp direct, les groupes, le resp virtuel, etc ...)
 // renvoit une liste de login entre quotes et séparés par des virgules
 $tab_all_users_du_hr = \hr\Fonctions::recup_infos_all_users_du_hr($_SESSION['userlogin']);
+asort($tab_all_users_du_hr);
 $tab_all_users_du_grand_resp = recup_infos_all_users_du_grand_resp($_SESSION['userlogin']);
+$list_group = get_list_groupes_pour_rh($_SESSION['userlogin']);
+if (!empty($list_group)) {
+    $sql_group = "SELECT g_gid, g_groupename FROM conges_groupe WHERE g_gid IN ($list_group) ORDER BY g_groupename "  ;
+    $resultatGroupe = \includes\SQL::singleton()->query($sql_group);
 
-if ( (count($tab_all_users_du_hr)!=0) || (count($tab_all_users_du_grand_resp)!=0) ) {
-    /************************************************************/
-    /* SAISIE GLOBALE pour tous les utilisateurs du responsable */
-    $return .= affichage_saisie_globale_pour_tous($tab_type_cong);
-    $return .= '<br>';
-
-    /***********************************************************************/
-    /* SAISIE GROUPE pour tous les utilisateurs d'un groupe du responsable */
-    $return .= affichage_saisie_globale_groupe($tab_type_cong);
-    $return .= '<br>';
-
-    /************************************************************/
-    /* SAISIE USER PAR USER pour tous les utilisateurs du responsable */
-    $return .= affichage_saisie_user_par_user($tab_type_cong, $tab_type_conges_exceptionnels, $tab_all_users_du_hr, $tab_all_users_du_grand_resp);
-    $return .= '<br>';
+    $groupes = $resultatGroupe->fetchAll();
 } else {
-    $return .= _('resp_etat_aucun_user') . '<br>';
+    $groupes = [];
 }
+
 
 require_once VIEW_PATH . 'HautResponsable/CreditConges.php';
