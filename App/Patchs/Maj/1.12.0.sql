5a201d751dac1cd0908bbc863e9d1903f5639814
wouldsmina <wouldsmina@gmail.com>
2018-09-12 22:52:17 +0200

    retrait options config admin

--- App/Patchs/Maj/1.12.0.sql
+++ App/Patchs/Maj/1.12.0.sql
@@ -1,1 +1,5 @@
 INSERT IGNORE INTO `conges_appli` VALUES ("version_last_maj", "");
+
+DELETE FROM conges_config WHERE conf_nom = 'affiche_bouton_config_pour_admin';
+DELETE FROM conges_config WHERE conf_nom = 'affiche_bouton_config_absence_pour_admin';
+DELETE FROM conges_config WHERE conf_nom = 'affiche_bouton_config_mail_pour_admin';
