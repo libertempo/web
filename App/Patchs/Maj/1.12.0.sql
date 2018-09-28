77a64df450cb9c2ff7ec67120dbbff7b1921ac95
Prytoegrian <5312739+prytoegrian@users.noreply.github.com>
2018-09-28 13:20:27 +0100

    Définition de la config API à chaque màj (#713)
    * Copying old vendor file
    
    * Maj API à chaque update

--- App/Patchs/Maj/1.12.0.sql
+++ App/Patchs/Maj/1.12.0.sql
@@ -1,3 +1,3 @@
-INSERT IGNORE INTO `conges_appli` VALUES ("version_last_maj", "");
+INSERT INTO `conges_appli` VALUES ("version_last_maj", "");
 DELETE FROM `conges_users` WHERE u_login = 'admin'; 
 UPDATE `conges_users` SET `u_is_admin`='Y' WHERE u_is_hr = 'Y'; 
