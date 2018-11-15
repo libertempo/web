2a3d13cefe30e560bb21740d8b7672a49bdf8d92
Prytoegrian <prytoegrian@protonmail.com>
2018-11-15 15:46:12 +0100

    Update version LT

--- Public/template/template_define.php
+++ Public/template/template_define.php
@@ -1,3 +1,3 @@
 <?php
 
-define('BOTTOM_TEXT','Libertempo 1.12 "Akhaten". <a href="https://github.com/Libertempo/Libertempo-web/issues">Un bug, une remarque <i class="fa fa-github fa-2x"></i></a>');
+define('BOTTOM_TEXT','Libertempo 1.13 "Nooma". <a href="https://github.com/Libertempo/Libertempo-web/issues">Un bug, une remarque <i class="fa fa-github fa-2x"></i></a>');
