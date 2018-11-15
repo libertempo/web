7d1328a887ac3b46ebddfe5398c86408739db5ff
Prytoegrian <prytoegrian@protonmail.com>
2018-11-15 15:46:12 +0100

    Update version LT

--- Public/template/template_define.php
+++ Public/template/template_define.php
@@ -1,3 +1,3 @@
 <?php
 
-define('BOTTOM_TEXT','Libertempo 1.12 "Akhaten".<br /><a href="https://feedback.libertempo.fr/">Rapport de bug ou amélioration <img src="' . IMG_PATH . '/Favicons/favicon-16x16.png" alt="logo-libertempo"/></a>');
+define('BOTTOM_TEXT','Libertempo 1.13 "Nooma". <a href="https://feedback.libertempo.fr/">Un bug, une remarque <img src="' . IMG_PATH . '/Favicons/favicon-16x16.png" alt="logo-libertempo"/></a>');
--- version.php
+++ version.php
@@ -4,6 +4,6 @@ defined( '_PHP_CONGES' ) or die( 'Restricted access' );
 
 // site et numero de version de PHP_CONGES
 // ne pas toucher ces variables SVP ;-)
-$config_php_conges_version="1.12";
+$config_php_conges_version="1.13";
 $config_url_site_web_php_conges="http://Libertempo.tuxfamily.org";
 // ne pas toucher ces variables SVP ;-)
