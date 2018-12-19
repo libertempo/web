392aa2ab2b4f88c3f38909e235f830bdc221fe32
Prytoegrian <prytoegrian@protonmail.com>
2018-12-20 00:03:14 +0100

    Minimal version 1.10

--- App/Patchs/Installation/1.10.0.sql
+++ App/Patchs/Installation/1.10.0.sql
@@ -549,3 +549,29 @@ INSERT IGNORE INTO `conges_config` (`conf_nom`, `conf_valeur`, `conf_groupe`, `c
 # Booléen qui indique si létablissement a besoin # des fonctionnalités de gestion des heures
 # additionnelles et de repos
 INSERT IGNORE INTO `conges_config` VALUES ("gestion_heures", "TRUE", "12_Fonctionnement de l\'Etablissement", "boolean", "config_comment_gestion_heures");
+
+
+# suppression de la variable print_disable_users de la bdd
+DELETE FROM conges_config WHERE conf_nom = 'print_disable_users';
+
+# suppression de loption calcul de nombre de jours, par défaut cest automatique
+DELETE FROM conges_config WHERE conf_nom = 'affiche_bouton_calcul_nb_jours_pris';
+DELETE FROM conges_config WHERE conf_nom = 'rempli_auto_champ_nb_jours_pris';
+
+# suppression de loption de gestion des goupes
+DELETE FROM conges_config WHERE conf_nom = 'gestion_groupes';
+
+# Ajout des champs de lutilisateur requis pour lAPI
+ALTER TABLE `conges_users`
+    ADD `date_inscription` DATETIME NOT NULL DEFAULT NOW(),
+    ADD `token` VARCHAR(100) NOT NULL DEFAULT "",
+    ADD `date_last_access` DATETIME NOT NULL,
+    ADD INDEX `token` (`token`);
+
+# Ajout du token dinstance
+INSERT IGNORE INTO `conges_appli` VALUES ("token_instance", "");
+
+# Modification de tous les mots de passe dutilisateurs non db_conges (md5(none))
+UPDATE `conges_users` SET u_passwd = "334c4a4c42fdb79d7ebc3e73b517e6f8" where u_passwd = "none";
+
+DELETE FROM conges_config WHERE conf_nom = 'disable_saise_champ_nb_jours_pris';
