9216012731ac721dbf585cdd340788031be8f692
Prytoegrian <prytoegrian@protonmail.com>
2018-12-24 12:30:46 +0100

    First template

--- hr/hr_ajout_groupe.php
+++ hr/hr_ajout_groupe.php
@@ -2,130 +2,7 @@
 defined('_PHP_CONGES') or die('Restricted access');
 $gestionGroupes = new \App\ProtoControllers\Groupe\Gestion();
 
-/**
- *
- * Formulaire d'ajout ou de modification d'un groupe
- *
- * @param int $idGroupe
- * @return string
- */
-function getForm($idGroupe = NIL_INT)
-{
-    $config = new \App\Libraries\Configuration(\includes\SQL::singleton());
-
-    $return = '';
-    $PHP_SELF = filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_URL);
-    $message = '';
-    $infosGroupe = [
-        'nom' => '',
-        'doubleValidation' => '',
-        'comment' => ''
-    ];
-    $data = NULL;
-
-    $errorsLst = [];
-    if (!empty($_POST)) {
-        if (0 >= (int) $this->postHtmlCommon($_POST, $errorsLst)) {
-            $errors = '';
-            if (!empty($errorsLst)) {
-                foreach ($errorsLst as $key => $value) {
-                    if (is_array($value)) {
-                        $value = implode(' / ', $value);
-                    }
-                    $errors .= '<li>' . $key . ' : ' . $value . '</li>';
-                }
-                $message = '<br><div class="alert alert-danger">' . _('erreur_recommencer') . '<ul>' . $errors . '</ul></div>';
-            }
-            $data = $this->FormData2Array($_POST);
-        } else {
-            if (key_exists('_METHOD', $_POST)) {
-                redirect(ROOT_PATH . 'hr/hr_index.php?onglet=liste_groupe&notice=update', false);
-            } else {
-                redirect(ROOT_PATH . 'hr/hr_index.php?onglet=liste_groupe&notice=insert', false);
-            }
-        }
-    }
-
-    if (isset($data)) {
-        $infosGroupe = [
-            'nom' => $data['nom'],
-            'comment' => $data['commentaire']
-        ];
-        if ($config->isDoubleValidationActive()) {
-            $infosGroupe['doubleValidation'] = $data['isDoubleValidation'];
-        }
-    } elseif (NIL_INT !== $idGroupe) {
-        $infosGroupe = \App\ProtoControllers\Groupe::getInfosGroupe($idGroupe, \includes\SQL::singleton());
-    }
-
-    $selectId = uniqid();
-    $DivGrandRespId = uniqid();
-    $return .= '<div onload="showDivGroupeGrandResp(\'' . $selectId . '\',\'' . $DivGrandRespId . '\');" class="form-group">';
-    if (NIL_INT !== $idGroupe) {
-        $return .= '<h1>' . _('admin_modif_groupe_titre') . '</h1>';
-    } else {
-        $return .= '<h1>' . _('admin_groupes_new_groupe') . '</h1>';
-    }
-
-    $return .= $message;
-    $return .= '<form method="post" action=""  role="form">';
-    $table = new \App\Libraries\Structure\Table();
-    $table->addClasses([
-        'table',
-    ]);
-    $childTable = '<thead><tr>';
-    $childTable .= '<th><b>' . _('Nom du groupe') . '</b></th>';
-    $childTable .= '<th>' . _('admin_groupes_libelle') . ' / ' . _('divers_comment_maj_1') . '</th>';
-    if ($config->isDoubleValidationActive()) {
-        $childTable .= '<th>' . _('admin_groupes_double_valid') . '</th>';
-    }
-    $childTable .= '</tr></thead><tbody>';
-    $childTable .= '<tr>';
-    $childTable .= '<td><input class="form-control" type="text" name="new_group_name" size="30" maxlength="50" value="' . $infosGroupe['nom'] . '" required></td>';
-    $childTable .= '<td><input class="form-control" type="text" name="new_group_libelle" size="50" maxlength="250" value="' . $infosGroupe['comment'] . '"></td>';
-    if ($config->isDoubleValidationActive()) {
-        $selectN = $infosGroupe['doubleValidation'] == 'N' ? 'selected="selected"' : '';
-        $selectY = $infosGroupe['doubleValidation'] == 'Y' ? 'selected="selected"' : '';
-        $childTable .= '<td><select class="form-control" name="new_group_double_valid" id="' . $selectId . '" onchange="showDivGroupeGrandResp(\'' . $selectId . '\',\'' . $DivGrandRespId . '\');"><option value="N" ' . $selectN . '>N</option><option value="Y" ' . $selectY . '>Y</option></select></td>';
-    }
-    $childTable .= '</tr></tbody>';
-    $table->addChild($childTable);
-    ob_start();
-    $table->render();
-    $return .= ob_get_clean();
-    $return .= '<hr>';
-
-    $return .= '<div class="row">';
-    $return .= '<div class="col-md-6">';
-    $return .= '<h2>' . _('admin_gestion_groupe_users_membres') . '</h2>';
-    $return .= getFormChoixEmploye($idGroupe, $data);
-    $return .= '</div>';
-
-    $return .= '<div class="col-md-6">';
-    $return .= '<h2>' . _('admin_gestion_groupe_resp_responsables') . '</h2>';
-    $return .= getFormChoixResponsable($idGroupe, $selectId, $data);
-    $return .= '</div>';
-
-    $return .= '<div class="col-md-6 hide" id="' . $DivGrandRespId . '">';
-    $return .= '<h2>' . _('admin_gestion_groupe_grand_resp_responsables') . '</h2>';
-    $return .= getFormChoixGrandResponsable($idGroupe, $selectId, $data);
-    $return .= '</div>';
-    $return .= '</div>';
-
-    $return .= '</div>';
-
-    $return .= '<div class="form-group">';
-    if (NIL_INT !== $idGroupe) {
-        $return .= '<input type="hidden" name="_METHOD" value="PUT" />';
-        $return .= '<input type="hidden" name="group" value="' . $idGroupe . '" />';
-    }
-    $return .= '<input class="btn btn-success" type="submit" value="' . _('form_submit') . '">';
-    $return .= '<a class="btn" href="' . $PHP_SELF . '?onglet=liste_groupe">' . _('form_annul') . '</a>';
-    $return .= '</div>';
-    $return .= '</form>';
-
-    return $return;
-}
+$idGroupe = NIL_INT;
 
 /**
  *
@@ -154,7 +31,7 @@ function getFormChoixEmploye($idGroupe, $data = NULL)
     $childTable .= '</thead>';
     $childTable .= '<tbody>';
     $i = true;
-    foreach ($this->getEmployes($idGroupe) as $login => $info) {
+    foreach (getEmployes($idGroupe) as $login => $info) {
         $inputOption = '';
 
         if (isset($data)) {
@@ -186,6 +63,33 @@ function getFormChoixEmploye($idGroupe, $data = NULL)
 
 /**
  *
+ * retournes les utilisateurs
+ * si $idGroupe existe, marquage des employés du groupe
+ *
+ * @param int $idGroupe
+ * @return array
+ */
+function getEmployes($idGroupe = NIL_INT)
+{
+    $employes = [];
+    $idsUtilisateurs = \App\ProtoControllers\Utilisateur::getListId(true);
+    foreach ($idsUtilisateurs as $login) {
+        $donnees = \App\ProtoControllers\Utilisateur::getDonneesUtilisateur($login);
+        $employes[$login] = [
+            'nom' => $donnees['u_nom'],
+            'prenom' => $donnees['u_prenom'],
+            'login' => $donnees['u_login'],
+            'isDansGroupe' => false
+        ];
+        if (NIL_INT != $idGroupe) {
+            $employes[$login]['isDansGroupe'] = \App\ProtoControllers\Groupe\Utilisateur::isUtilisateurDansGroupe($login, $idGroupe, \includes\SQL::singleton());
+        }
+    }
+    return $employes;
+}
+
+/**
+ *
  * Formulaire de selection du responsable d'un groupe
  *
  * @param int $idGroupe
@@ -211,7 +115,7 @@ function getFormChoixResponsable($idGroupe, $selectId, $data)
     $childTable .= '</thead>';
     $childTable .= '<tbody>';
     $i = true;
-    foreach ($this->getInfosResponsables($idGroupe) as $login => $info) {
+    foreach (getInfosResponsables($idGroupe) as $login => $info) {
         $inputOption = '';
 
         if (isset($data)) {
@@ -347,6 +251,61 @@ function getGrandResponsables($idGroupe = NIL_INT)
         }
     }
     return $responsables;
+}
+
+$config = new \App\Libraries\Configuration(\includes\SQL::singleton());
+
+$return = '';
+$PHP_SELF = filter_input(INPUT_SERVER, 'PHP_SELF', FILTER_SANITIZE_URL);
+$message = '';
+$infosGroupe = [
+    'nom' => '',
+    'doubleValidation' => '',
+    'comment' => ''
+];
+$data = NULL;
+
+$errorsLst = [];
+if (!empty($_POST)) {
+    if (0 >= (int) $this->postHtmlCommon($_POST, $errorsLst)) {
+        $errors = '';
+        if (!empty($errorsLst)) {
+            foreach ($errorsLst as $key => $value) {
+                if (is_array($value)) {
+                    $value = implode(' / ', $value);
+                }
+                $errors .= '<li>' . $key . ' : ' . $value . '</li>';
+            }
+            $message = '<br><div class="alert alert-danger">' . _('erreur_recommencer') . '<ul>' . $errors . '</ul></div>';
+        }
+        $data = $this->FormData2Array($_POST);
+    } else {
+        if (key_exists('_METHOD', $_POST)) {
+            redirect(ROOT_PATH . 'hr/hr_index.php?onglet=liste_groupe&notice=update', false);
+        } else {
+            redirect(ROOT_PATH . 'hr/hr_index.php?onglet=liste_groupe&notice=insert', false);
+        }
+    }
+}
+
+if (isset($data)) {
+    $infosGroupe = [
+        'nom' => $data['nom'],
+        'comment' => $data['commentaire']
+    ];
+    if ($config->isDoubleValidationActive()) {
+        $infosGroupe['doubleValidation'] = $data['isDoubleValidation'];
+    }
+} elseif (NIL_INT !== $idGroupe) {
+    $infosGroupe = \App\ProtoControllers\Groupe::getInfosGroupe($idGroupe, \includes\SQL::singleton());
+}
+
+$selectId = uniqid();
+$DivGrandRespId = uniqid();
+if (NIL_INT !== $idGroupe) {
+    $titre = '<h1>' . _('admin_modif_groupe_titre') . '</h1>';
+} else {
+    $titre = '<h1>' . _('admin_groupes_new_groupe') . '</h1>';
 }
 
 require_once VIEW_PATH . 'Groupe/Edition.php';
