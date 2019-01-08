a9bfc7406ef5daee17b89b1a5efdc4b1aa6ad348
Prytoegrian <prytoegrian@protonmail.com>
2019-01-08 12:49:57 +0100

    Icon

--- Public/template/template_define.php
+++ Public/template/template_define.php
@@ -1,3 +1,3 @@
 <?php
 
-define('BOTTOM_TEXT','Libertempo 1.13 "Nooma". <a href="https://feedback.libertempo.fr/">Un bug, une idée <i class="fa fa-github fa-2x"></i></a>');
+define('BOTTOM_TEXT','Libertempo 1.13 "Nooma". <a href="https://feedback.libertempo.fr/">Un bug, une idée <i class="fa fa-comment-o fa-2x"></i></a>');
--- includes/lang_profile.php
+++ includes/lang_profile.php
@@ -1,13 +1,12 @@
 <?php
 
-if (isset($_REQUEST['session']))
+if (isset($_REQUEST['session'])) {
 	session_is_valid($_REQUEST['session']);
-
+}
 
 $lang = 'fr_FR';
 putenv('LANG='.$lang); // On modifie la variable d'environnement
 $LoadLang = setlocale(LC_ALL, $lang, $lang.".utf8");
-
 $nomDesFichiersDeLangue = 'php-conges'; // Le nom de nos fichiers .mo
 bindtextdomain($nomDesFichiersDeLangue, LOCALE_PATH ); // On indique le chemin vers les fichiers .mo
 bind_textdomain_codeset($nomDesFichiersDeLangue, 'UTF-8');  // Nos fichiers de langue sont en UTF-8
