bd5b5dd3e67d6129b8a0ab87d07ec98973529317
Prytoegrian <prytoegrian@protonmail.com>
2019-03-29 22:13:55 +0100

    affiche demandes calendrier

--- App/Patchs/Maj/1.13.6.sql
+++ App/Patchs/Maj/1.13.6.sql
@@ -3,3 +3,4 @@ DELETE FROM `conges_config` WHERE conf_nom = 'auth';
 DELETE FROM `conges_config` WHERE conf_nom = 'user_saisie_demande';
 DELETE FROM `conges_config` WHERE conf_nom = 'user_saisie_mission';
 DELETE FROM `conges_config` WHERE conf_nom = 'admin_see_all';
+DELETE FROM `conges_config` WHERE conf_nom = 'affiche_demandes_dans_calendrier';
