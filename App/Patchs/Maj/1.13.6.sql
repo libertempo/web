6a4e8f53a6abe5664537ec39b2d48045e6321e93
Prytoegrian <prytoegrian@protonmail.com>
2019-03-29 22:12:45 +0100

    admin see all

--- App/Patchs/Maj/1.13.6.sql
+++ App/Patchs/Maj/1.13.6.sql
@@ -2,3 +2,4 @@ DELETE FROM `conges_config` WHERE conf_nom = 'editions_papier';
 DELETE FROM `conges_config` WHERE conf_nom = 'auth';
 DELETE FROM `conges_config` WHERE conf_nom = 'user_saisie_demande';
 DELETE FROM `conges_config` WHERE conf_nom = 'user_saisie_mission';
+DELETE FROM `conges_config` WHERE conf_nom = 'admin_see_all';
