4ef06351437a3fabc237fb78f6cbec96eac8a6d3
Prytoegrian <prytoegrian@protonmail.com>
2019-03-30 15:29:53 +0100

    w/ view

--- App/Views/HautResponsable/Employe/Ajout.php
+++ App/Views/HautResponsable/Employe/Ajout.php
@@ -1,1 +1,95 @@
 <?php declare(strict_types = 1);
+/**
+ * $message
+ * $soldeHeureId
+ * $readOnly
+ * $optLdap
+ */
+?>
+
+<h1><?= _('Nouvel Utilisateur') ?></h1>
+<?= $message ?>
+
+<form id="manageUser" action="" method="post" accept-charset="UTF-8"
+enctype="application/x-www-form-urlencoded" class="form-group">
+    <table class="table table-hover table-responsive table-striped table-condensed">
+        <thead>
+            <tr>
+                <th><?= _('Identifiant') ?></th>
+                <th><?= _('Nom') ?></th>
+                <th><?= _('Prénom') ?></th>
+                <th><?= _('Quotité') ?></th>
+                <?php if ($config->isHeuresAutorise()) : ?>
+                    <th><?= _('solde d\'heure') ?></th>
+                <?php endif; ?>
+                <th><?= _('Responsable?') ?></th>
+                <th><?= _('Administrateur?') ?></th>
+                <th><?= _('Haut responsable?') ?></th>
+                <th><?= _('activé?') ?></th>
+                <?php if (!$config->isUsersExportFromLdap()) : ?>
+                    <th><?= _('Email') ?></th>
+                <?php endif; ?>
+                <?php if ($config->getHowToConnectUser() == "dbconges") : ?>
+                    <th><?= _('mot de passe') ?></th>
+                    <th><?= _('ressaisir mot de passe') ?></th>
+                <?php endif ?>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="update-line">
+                <td><input class="form-control" type="text" name="new_login" size="10" maxlength="99" value="<?= $formValue['login'] ?>" "<?= $readOnly ?>" required></td>
+                <td>
+                    <input class="form-control" type="text" id="new_nom" name="new_nom" size="10" maxlength="30" value="<?= $formValue['nom'] ?>" "<?= $optLdap ?>" required>
+                    <ul class="suggestions" id="suggestions"></ul>
+                </td>
+                <td><input class="form-control" type="text" name="new_prenom" size="10" maxlength="30" value="<?= $formValue['prenom'] ?>" "<?= $readOnly ?>" required></td>
+                <td><input class="form-control" type="text" name="new_quotite" size="3" maxlength="3" value="<?= $formValue['quotite'] ?>" required></td>
+                <?php if ($config->isHeuresAutorise()) : ?>
+                    <td>
+                        <input class="form-control" type="text" name="new_solde_heure" id="<?= $soldeHeureId ?>" size="6" maxlength="6" value="<?= $formValue['soldeHeure'] ?>">
+                    </td>
+                <?php endif; ?>
+                <td>
+                    <select class="form-control" name="new_is_resp">
+                        <option value="N" <?= $formValue['isResp'] == 'N' ? 'selected' : ''?>>N</option>
+                        <option value="Y" <?= $formValue['isResp'] == 'Y' ? 'selected' : ''?>>Y</option>
+                    </select>
+                </td>
+                <td>
+                    <select class="form-control" name="new_is_admin">
+                        <option value="N" <?= $formValue['isAdmin'] == 'N' ? 'selected' : ''?>>N</option>
+                        <option value="Y" <?= $formValue['isAdmin'] == 'Y' ? 'selected' : ''?>>Y</option>
+                    </select>
+                </td>
+                <td>
+                    <select class="form-control" name="new_is_hr">
+                        <option value="N" <?= $formValue['isHR'] == 'N' ? 'selected' : ''?>>N</option>
+                        <option value="Y" <?= $formValue['isHR'] == 'Y' ? 'selected' : ''?>>Y</option>
+                    </select>
+                </td>
+                <td>
+                    <select class="form-control" name="new_is_active">
+                        <option value="Y" <?= $formValue['isActive'] == 'Y' ? 'selected' : '' ?>>Y</option>
+                        <option value="N" <?= $formValue['isActive'] == 'N' ? 'selected' : '' ?>>N</option>
+                    </select>
+                </td>
+                <?php if (!$config->isUsersExportFromLdap()) : ?>
+                    <td><input class="form-control" type="text" name="new_email" size="10" maxlength="99" value="<?= $formValue['email'] ?>"></td>
+                <?php endif ;?>
+                <?php if ($config->getHowToConnectUser() == "dbconges") : ?>
+                    <td><input class="form-control" type="password" name="new_password1" size="10" maxlength="15" value="" autocomplete="off"></td>
+                    <td><input class="form-control" type="password" name="new_password2" size="10" maxlength="15" value="" autocomplete="off"></td>
+                <?php endif ;?>
+            </tr>
+        </tbody>
+        <script type="text/javascript">
+            generateTimePicker("<?= $soldeHeureId ?>");
+        </script>
+    </table>
+    <br><hr>
+    <?= getFormUserSoldes($formValue); ?>
+    <br><hr>
+    <?= getFormUserGroupes($formValue); ?>
+    <hr>
+    <input class="btn btn-success" type="submit" value="<?= _('form_submit') ?>">
+</form>
--- hr/hr_ajout_user.php
+++ hr/hr_ajout_user.php
@@ -1,146 +1,325 @@
 <?php declare(strict_types = 1);
 
 defined('_PHP_CONGES') or die('Restricted access');
-echo getFormUser();
 
-/**
- * formulaire d'ajout / modification d'un utilisateur
- *
- * @return string
- */
-function getFormUser()
-{
-    $return    = '';
-    $message   = '';
+$message   = '';
+$sql = \includes\SQL::singleton();
+$config = new \App\Libraries\Configuration($sql);
+$formValue = [
+    'login' => '',
+    'nom' => '',
+    'prenom' => '',
+    'quotite' => '100',
+    'soldeHeure' => '00:00',
+    'isResp' => 'N',
+    'isAdmin' => 'N',
+    'isHR' => 'N',
+    'isActive' => 'Y',
+    'email' => '',
+    'pwd1' => '',
+    'pwd2' => '',
+    'groupesId' => [],
+];
+
+if (!empty($_POST)) {
     $errorsLst = [];
     $notice    = '';
+    $formValue = dataForm2Array($_POST, $sql, $config);
+    if (postFormUtilisateur($formValue, $errorsLst, $notice)) {
+        redirect(ROOT_PATH . 'hr/page_principale?notice=' . $notice, false);
+    } else {
+        if (!empty($errorsLst)) {
+            $errors = '';
+            foreach ($errorsLst as $key => $value) {
+                $errors .= '<li>' . $key . ' : ' . $value . '</li>';
+            }
+            $message = '<div class="alert alert-danger">' . _('erreur_recommencer') . ' :<ul>' . $errors . '</ul></div>';
+        }
+    }
+}
+
+$soldeHeureId = uniqid();
+$readOnly = '';
+$optLdap = '';
+if ($config->isUsersExportFromLdap()) {
+    $readOnly = 'readonly';
+    $optLdap = 'onkeyup="searchLdapUser()" autocomplete="off"';
+}
+
+/**
+ * Nettoyage des données postés par le formulaire
+ *
+ * @param array $htmlPost
+ * @param \includes\SQL $sql
+ * @param \App\Libraries\Configuration $config
+ *
+ * @return array
+ */
+function dataForm2Array(array $htmlPost, \includes\SQL $sql, \App\Libraries\Configuration $config) : array
+{
+    $data['login'] = htmlentities($htmlPost['new_login'], ENT_QUOTES | ENT_HTML401);
+    $data['oldLogin'] = key_exists('old_login', $htmlPost)
+            ? htmlentities($htmlPost['old_login'], ENT_QUOTES | ENT_HTML401)
+            : htmlentities($htmlPost['new_login'], ENT_QUOTES | ENT_HTML401);
+    $data['nom'] = htmlentities($htmlPost['new_nom'], ENT_QUOTES | ENT_HTML401);
+    $data['prenom'] = htmlentities($htmlPost['new_prenom'], ENT_QUOTES | ENT_HTML401);
+    $data['quotite'] = (int) $htmlPost['new_quotite'];
+    $data['soldeHeure'] = htmlentities($htmlPost['new_solde_heure'], ENT_QUOTES | ENT_HTML401);
+    $data['isActive'] = $htmlPost['new_is_active'] === 'N' ? 'N' : 'Y';
+    $data['isResp'] = $htmlPost['new_is_resp'] === 'Y' ? 'Y' : 'N';
+    $data['isAdmin'] = $htmlPost['new_is_admin'] === 'Y' ? 'Y' : 'N';
+    $data['isHR'] = $htmlPost['new_is_hr'] === 'Y' ? 'Y' : 'N';
+
+    if (!$config->isUsersExportFromLdap()) {
+        $data['email'] = htmlentities($htmlPost['new_email'], ENT_QUOTES | ENT_HTML401);
+    } else {
+        $injectableCreator = new \App\Libraries\InjectableCreator($sql, $config);
+        $ldap = $injectableCreator->get(\App\Libraries\Ldap::class);
+        $data['email'] = $ldap->getEmailUser($data['login']);
+    }
+
+    if ($config->getHowToConnectUser() == "dbconges") {
+        $data['pwd1'] = $htmlPost['new_password1'] == "" ? "" : md5($htmlPost['new_password1']);
+        $data['pwd2'] = $htmlPost['new_password2'] == "" ? "" : md5($htmlPost['new_password2']);
+    } else {
+        $data['pwd1'] = md5(uniqid('', true));
+        $data['pwd2'] = md5('none');
+    }
+
+    if (array_key_exists('_METHOD', $htmlPost)) {
+        $data['_METHOD'] = htmlentities($htmlPost['_METHOD'], ENT_QUOTES | ENT_HTML401);
+    }
+
+    foreach ($htmlPost['tab_new_jours_an'] as $typeId => $joursAn) {
+        $tmp = htmlentities($joursAn, ENT_QUOTES | ENT_HTML401);
+        $data['joursAn'][$typeId] = strtr((string) \App\Helpers\Formatter::roundToHalf($tmp), ",", ".");
+    }
+    foreach ($htmlPost['tab_new_solde'] as $typeId => $solde) {
+        $tmp = htmlentities($solde, ENT_QUOTES | ENT_HTML401);
+        $data['soldes'][$typeId] = strtr((string) \App\Helpers\Formatter::roundToHalf($tmp), ",", ".");
+    }
+    foreach ($htmlPost['tab_new_reliquat'] as $typeId => $reliquat) {
+        $tmp = htmlentities($reliquat, ENT_QUOTES | ENT_HTML401);
+        $data['reliquats'][$typeId] = strtr((string) \App\Helpers\Formatter::roundToHalf($tmp), ",", ".");
+    }
+    $data['groupesId'] = array_key_exists('checkbox_user_groups', $htmlPost) ? array_keys($htmlPost['checkbox_user_groups']) : [];
+
+    return $data;
+}
+
+/**
+ * Traite la creation d'un utilisateur
+ *
+ * @param array $post
+ * @param array &$errors
+ * @param string $notice
+ *
+ * @return int
+ */
+function postFormUtilisateur(array $post, array &$errors, string &$notice) : bool
+{
+    $return = false;
+    if (!\App\ProtoControllers\Utilisateur::isRH($_SESSION['userlogin'])) {
+        $errors[] = _('non autorisé');
+        return $return;
+    }
+
+    $return = insertUtilisateur($post, $errors);
+    if ($return) {
+        $notice = "inserted";
+        log_action(0, '', $post['login'], 'utilisateur ' . $post['login'] . ' ajouté');
+    }
+    return $return;
+}
+
+/**
+ * Création d'un nouvel utilisateur
+ *
+ * @param array $data
+ * @param array $errors
+ * @return boolean
+ */
+function insertUtilisateur(array $data, array &$errors) : bool
+{
     $sql = \includes\SQL::singleton();
     $config = new \App\Libraries\Configuration($sql);
-    $formValue = [
-        'login' => '',
-        'nom' => '',
-        'prenom' => '',
-        'quotite' => '100',
-        'soldeHeure' => '00:00',
-        'isResp' => 'N',
-        'isAdmin' => 'N',
-        'isHR' => 'N',
-        'isActive' => 'Y',
-        'email' => '',
-        'pwd1' => '',
-        'pwd2' => '',
-        'groupesId' => [],
-    ];
+    if (!isFormInsertValide($data, $errors, $sql, $config)) {
+        return false;
+    }
 
-    if (!empty($_POST)) {
-        $formValue = static::dataForm2Array($_POST, $sql, $config);
-        if (static::postFormUtilisateur($formValue, $errorsLst, $notice)) {
-            redirect(ROOT_PATH . 'hr/page_principale?notice=' . $notice, false);
-        } else {
-            if (!empty($errorsLst)) {
-                $errors = '';
-                foreach ($errorsLst as $key => $value) {
-                    $errors .= '<li>' . $key . ' : ' . $value . '</li>';
-                }
-                $message = '<div class="alert alert-danger">' . _('erreur_recommencer') . ' :<ul>' . $errors . '</ul></div>';
-            }
+    $sql->getPdoObj()->begin_transaction();
+    $insertInfos = insertInfosUtilisateur($data, $sql);
+    $insertSoldes = insertSoldeUtilisateur($data, $sql);
+    $insertGroupes = true;
+    if (!empty($data['groupesId'])) {
+        $insertGroupes = insertGroupesUtilisateur($data, $sql);
+    }
+    if ($insertInfos && $insertSoldes && $insertGroupes) {
+        return $sql->getPdoObj()->commit();
+    }
+
+    $sql->getPdoObj()->rollback();
+    return false;
+}
+
+function insertInfosUtilisateur(array $data, \includes\SQL $sql)
+{
+    $req = "INSERT INTO conges_users SET
+                u_login='" . $data['login'] . "',
+                u_nom='" . $data['nom'] . "',
+                u_prenom='" . $data['prenom'] . "',
+                u_is_resp='" . $data['isResp'] . "',
+                u_is_admin='" . $data['isAdmin'] . "',
+                planning_id = 0,
+                u_is_hr='" . $data['isHR'] . "',
+                u_passwd='" . $data['pwd1'] . "',
+                u_quotite=" . $data['quotite'] . ",
+                u_email = '" . $data['email'] . "',
+                u_heure_solde=" . \App\Helpers\Formatter::hour2Time($data['soldeHeure']) . ",
+                date_inscription = '" . date('Y-m-d H:i') . "';";
+
+    return $sql->query($req);
+}
+
+function insertSoldeUtilisateur(array $data, \includes\SQL $sql) : bool
+{
+    $config = new \App\Libraries\Configuration($sql);
+    $typeAbsencesConges = \App\ProtoControllers\Conge::getTypesAbsences($sql, 'conges');
+
+    foreach ($typeAbsencesConges as $typeId => $info) {
+        $valuesStd[] = "('" . $data['login'] . "' ,"
+                            . $typeId . ", "
+                            . $data['joursAn'][$typeId] . ", "
+                            . $data['soldes'][$typeId] . ", "
+                            . $data['reliquats'][$typeId] . ")" ;
+    }
+    $req = "INSERT INTO conges_solde_user (su_login, su_abs_id, su_nb_an, su_solde, su_reliquat) VALUES " . implode(",", $valuesStd);
+    $returnStd = $sql->query($req);
+    $returnExc = 1;
+    if ($config->isCongesExceptionnelsActive()) {
+        $typeAbsencesExceptionnels = \App\ProtoControllers\Conge::getTypesAbsences($sql, 'conges_exceptionnels');
+        foreach ($typeAbsencesExceptionnels as $typeId => $info) {
+            $valuesExc[] = "('" . $data['login'] . "' ,"
+                                . $typeId . ", 0, "
+                                . $data['soldes'][$typeId] . ", 0)" ;
+
+        }
+        $req = "INSERT INTO conges_solde_user (su_login, su_abs_id, su_nb_an, su_solde, su_reliquat) VALUES " . implode(",", $valuesExc);
+        $returnExc = $sql->query($req);
+    }
+
+    return $returnStd && $returnExc;
+}
+
+function insertGroupesUtilisateur(array $data, \includes\SQL $sql)
+{
+    foreach ($data['groupesId'] as $gid) {
+        $values[] = "(" . $gid . ", '" . $data['login'] . "')"  ;
+    }
+    $req = "INSERT INTO conges_groupe_users (gu_gid, gu_login) VALUES " . implode(",", $values);
+
+    return $sql->query($req);
+}
+
+/**
+ * Controle la conformité du formulaire de création
+ *
+ * @param array $data
+ * @param array $errors
+ * @param \includes\SQL $sql
+ * @param \App\Libraries\Configuration $config
+ *
+ * @return boolean
+ */
+function isFormInsertValide(array $data, array &$errors, \includes\SQL $sql, \App\Libraries\Configuration $config) : bool
+{
+    $return = true;
+    $users = \App\ProtoControllers\Utilisateur::getListId(false);
+    if (in_array($data['login'], $users)) {
+        $errors[] = _('Cet identifiant existe déja.');
+        $return = false;
+    }
+
+    if ($config->getHowToConnectUser() == 'dbconges') {
+        if ($data['pwd1'] == '' || strcmp($data['pwd1'], $data['pwd2'])!=0 ) {
+            $errors[] = _('Saisie du mot de passe incorrect');
+            $return = false;
         }
     }
 
+    return $return && isFormValide($data, $errors, $sql, $config);
+}
 
-    $return .= '<h1>' . _('Nouvel Utilisateur') . '</h1>';
-    $return .= $message;
+/**
+ * Controle la conformité du formulaire (création et mise à jour)
+ *
+ * @param array $data
+ * @param array $errors
+ * @param \includes\SQL $sql
+ * @param \App\Libraries\Configuration $config
+ * @return boolean
+ */
+function isFormValide(array $data, array &$errors, \includes\SQL $sql, \App\Libraries\Configuration $config) : bool
+{
+    $return = true;
 
-    $return .= '<form id="manageUser" action="" method="post" accept-charset="UTF-8"
-enctype="application/x-www-form-urlencoded" class="form-group">';
-    $table = new \App\Libraries\Structure\Table();
-    $table->addClasses([
-        'table',
-        'table-hover',
-        'table-responsive',
-        'table-striped',
-        'table-condensed'
-    ]);
-
-    $childTable = '<thead>';
-    $childTable .= '<tr>';
-    $childTable .= '<th>' . _('Identifiant') . '</th>';
-    $childTable .= '<th>' . _('Nom') . '</th>';
-    $childTable .= '<th>' . _('Prénom') . '</th>';
-    $childTable .= '<th>' . _('Quotité') . '</th>';
-    if ($config->isHeuresAutorise()) {
-        $childTable .= '<th>' . _('solde d\'heure') . '</th>';
-    }
-    $childTable .= '<th>' . _('Responsable?') . '</th>';
-    $childTable .= '<th>' . _('Administrateur?') . '</th>';
-    $childTable .= '<th>' . _('Haut responsable?') . '</th>';
-    $childTable .= '<th>' . _('activé?') . '</th>';
-    if (!$config->isUsersExportFromLdap()) {
-        $childTable .= '<th>' . _('Email') . '</th>';
-    }
-    if ($config->getHowToConnectUser() == "dbconges") {
-        $childTable .= '<th>' . _('mot de passe') . '</th>';
-        $childTable .= '<th>' . _('ressaisir mot de passe') . '</th>';
-    }
-    $childTable .= '</tr></thead><tbody>';
-    $soldeHeureId = uniqid();
-    $readOnly = '';
-    $optLdap = '';
-    if ($config->isUsersExportFromLdap()) {
-        $readOnly = 'readonly';
-        $optLdap = 'onkeyup="searchLdapUser()" autocomplete="off"';
+    if (!preg_match('/^[@a-z.\d_-]{2,30}$/i', $data['login'])) {
+        $errors[] = _('Identifiant incorrect.');
+        $return = false;
     }
 
-    $childTable .= '<tr class="update-line">';
+    if ('' == $data['nom']) {
+        $errors[] = _('Veuillez saisir un nom');
+        $return = false;
+    }
 
-    $childTable .= "<td><input class=\"form-control\" type=\"text\" name=\"new_login\" size=\"10\" maxlength=\"99\" value=\"".$formValue['login']."\" " . $readOnly . " required></td>" ;
-    $childTable .= "<td><input class=\"form-control\" type=\"text\" id=\"new_nom\" name=\"new_nom\" size=\"10\" maxlength=\"30\" value=\"".$formValue['nom']."\" " . $optLdap . " required>
-                    <ul class=\"suggestions\" id=\"suggestions\"></ul></td>" ;
-    $childTable .= "<td><input class=\"form-control\" type=\"text\" name=\"new_prenom\" size=\"10\" maxlength=\"30\" value=\"".$formValue['prenom']."\" " . $readOnly . " required></td>" ;
-    $childTable .= "<td><input class=\"form-control\" type=\"text\" name=\"new_quotite\" size=\"3\" maxlength=\"3\" value=\"".$formValue['quotite']."\" required></td>" ;
+    if ('' == $data['prenom']) {
+        $errors[] = _('Veuillez saisir un prenom');
+        $return = false;
+    }
+
+    if (0 >= $data['quotite'] || 100 < $data['quotite']) {
+        $errors[] = _('quotité incorrect');
+        $return = false;
+    }
 
     if ($config->isHeuresAutorise()) {
-        $childTable .= "<td><input class=\"form-control\" type=\"text\" name=\"new_solde_heure\" id=\"" . $soldeHeureId . "\" size=\"6\" maxlength=\"6\" value=\"".$formValue['soldeHeure']."\"></td>" ;
-    } else {
-        $childTable .= "<input class=\"form-control\" type=\"hidden\" name=\"new_solde_heure\" id=\"" . $soldeHeureId . "\" size=\"6\" maxlength=\"6\" value=\"0\">" ;
+        if (!\App\Helpers\Formatter::isHourFormat($data['soldeHeure'])) {
+            $errors[] = _('Format du solde d\'heure incorrect');
+            $return = false;
+        }
     }
-    $childTable .= "<td><select class=\"form-control\" name=\"new_is_resp\" >
-                    <option value=\"N\" " . ($formValue['isResp'] == 'N' ? 'selected' : '') . ">N</option>
-                    <option value=\"Y\" " . ($formValue['isResp'] == 'Y' ? 'selected' : '') . ">Y</option></select></td>" ;
-    $childTable .= "<td><select class=\"form-control\" name=\"new_is_admin\" >
-                    <option value=\"N\" " . ($formValue['isAdmin'] == 'N' ? 'selected' : '') . ">N</option>
-                    <option value=\"Y\" " . ($formValue['isAdmin'] == 'Y' ? 'selected' : '') . ">Y</option></select></td>" ;
-    $childTable .= "<td><select class=\"form-control\" name=\"new_is_hr\" >
-                    <option value=\"N\" " . ($formValue['isHR'] == 'N' ? 'selected' : '') . ">N</option>
-                    <option value=\"Y\" " . ($formValue['isHR'] == 'Y' ? 'selected' : '') . ">Y</option></select></td>" ;
-    $childTable .= "<td><select class=\"form-control\" name=\"new_is_active\" >
-                    <option value=\"Y\" " . ($formValue['isActive'] == 'Y' ? 'selected' : '') . ">Y</option>
-                    <option value=\"N\" " . ($formValue['isActive'] == 'N' ? 'selected' : '') . ">N</option></select></td>" ;
 
     if (!$config->isUsersExportFromLdap()) {
-        $childTable .= "<td><input class=\"form-control\" type=\"text\" name=\"new_email\" size=\"10\" maxlength=\"99\" value=\"".$formValue['email']."\"></td>" ;
+        if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors[] = _('Format de l\'adresse email incorrect');
+            $return = false;
+        }
     }
-    if ($config->getHowToConnectUser() == "dbconges") {
-        $childTable .= "<td><input class=\"form-control\" type=\"password\" name=\"new_password1\" size=\"10\" maxlength=\"15\" value=\"\" autocomplete=\"off\"></td>" ;
-        $childTable .= "<td><input class=\"form-control\" type=\"password\" name=\"new_password2\" size=\"10\" maxlength=\"15\" value=\"\" autocomplete=\"off\"></td>" ;
+
+    foreach ($data['joursAn'] as $typeId => $joursAn) {
+        if (!is_numeric($joursAn)) {
+            $errors[] = _('nombre de jours par an incorrect');
+            $return = false;
+            break;
+        }
     }
-    $childTable .= '</tr></tbody>';
-    $childTable .= '<script type="text/javascript">generateTimePicker("' . $soldeHeureId . '");</script>';
-    $table->addChild($childTable);
-    ob_start();
-    $table->render();
-    $return .= ob_get_clean();
-    $return .= '<br><hr>';
 
-    $return .= static::getFormUserSoldes($formValue);
-    $return .= '<br><hr>';
+    foreach ($data['soldes'] as $typeId => $solde) {
+        if (!is_numeric($solde)) {
+            $errors[] = _('solde incorrect');
+            $return = false;
+            break;
+        }
+    }
 
-    $return .= getFormUserGroupes($formValue);
-    $return .= '<hr>';
-
-    $return .= '<input class="btn btn-success" type="submit" value="' . _('form_submit') . '">';
-    $return .= ' <a class="btn btn-default" href="page_principale">' . _('form_cancel') . '</a>';
-    $return .= '</form>';
+    foreach ($data['reliquats'] as $typeId => $reliquat) {
+        if (!is_numeric($reliquat)) {
+            $errors[] = _('reliquat incorrect');
+            $return = false;
+            break;
+        }
+    }
 
     return $return;
 }
@@ -153,7 +332,7 @@ enctype="application/x-www-form-urlencoded" class="form-group">';
  * @return string
  *
  */
-function getFormUserSoldes($data)
+function getFormUserSoldes(array $data) : string
 {
     $sql = \includes\SQL::singleton();
     $config = new \App\Libraries\Configuration($sql);
@@ -221,7 +400,7 @@ function getFormUserSoldes($data)
  * @param array $data
  * @return string
  */
-function getFormUserGroupes($data)
+function getFormUserGroupes(array $data) : string
 {
     $sql = \includes\SQL::singleton();
     $return = '';
@@ -271,3 +450,5 @@ function getFormUserGroupes($data)
 
     return $return;
 }
+
+require_once VIEW_PATH . 'HautResponsable/Employe/Ajout.php';
