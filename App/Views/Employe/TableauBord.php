771298153909f2ded0f61f4396f8e7dd8c3c39f3
wouldsmina <wouldsmina@gmail.com>
2019-11-11 21:25:09 +0100

    comment

--- App/Views/Employe/TableauBord.php
+++ App/Views/Employe/TableauBord.php
@@ -1,9 +1,7 @@
 <?php declare(strict_types = 1);
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * $baseURIApi
  */
 
 ?>
