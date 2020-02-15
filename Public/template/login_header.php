9c3ab91961485d0c796a48eb02488aa999df7874
benrubson <6764151+benrubson@users.noreply.github.com>
2020-02-15 11:26:23 +0100

    @wouldsmina review

--- Public/template/login_header.php
+++ Public/template/login_header.php
@@ -3,5 +3,5 @@ include_once 'header.php';
 ?>
     <body class="login">
         <header>
-            <h1 class="login-heading"><img src="<?= IMG_PATH ?><?= $title ?>143x39.png" alt="<?= $title ?>"></h1>
+            <h1 class="login-heading"><img src="<?= IMG_PATH ?>Libertempo143x39.png" alt="<?= $title ?>"></h1>
         </header>
